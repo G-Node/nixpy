def5c5f306a389fee13e19f5c5131dac75807321
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2014-06-06 15:21:25 +0200

    setup.py: specify runtime_library_dirs
    This avoids the necessity to set LD_LIBRARY_PATH or RPATH in order to locate depending libraries.

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 __author__ = 'gicmo'
 
-from setuptools import setup, Extension
-from distutils.core import setup, Extension
+try:
+    from setuptools import setup, Extension
+except:
+    from distutils.core import setup, Extension
+
 import commands
 import numpy as np
 import sys
@@ -22,7 +25,8 @@ native_ext = Extension('nix.core',
                        extra_link_args=[boost_lnk_arg, nix_lnk_arg],
                        sources = ['src/core.cc'],
                        library_dirs = [nix_lib_dir, boost_lib_dir],
-                       include_dirs = [nix_inc_dir, boost_inc_dir, np.get_include()])
+                       include_dirs = [nix_inc_dir, boost_inc_dir, np.get_include()],
+                       runtime_library_dirs = [nix_lib_dir, boost_lib_dir])
 
 setup(name             = 'nix',
       version          = 0.1,
