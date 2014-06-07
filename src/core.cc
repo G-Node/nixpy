120d0393025c2025217f4259fd6882076d431841
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2014-06-07 14:17:27 +0200

    Create separate file for tranmodifying functions
    - Create dir 'include' and add it to setup.py
    - Create header include/transmodify.hpp
    - Create namespace nixpy

--- src/core.cc
+++ src/core.cc
@@ -1,7 +1,8 @@
 #include <boost/python.hpp>
 #include <boost/optional/optional.hpp>
-#include <nix.hpp>
 
+#include <nix.hpp>
+#include <transmorgify.hpp>
 
 static nix::File
 nix_file_open(std::string path)
@@ -21,57 +22,6 @@ nix_block_data_arrays(const nix::Block &b)
   return b.dataArrays();
 }
 
-template<typename T>
-struct vector_transmogrify {
-  static PyObject* convert(const std::vector<T>& vec) {
-    boost::python::list l = boost::python::list();
-
-    for(auto& item : vec) {
-      l.append(item);
-    }
-
-    return boost::python::incref(l.ptr());
-  }
-};
-
-template<typename T>
-struct option_transmogrify {
-  static PyObject* convert(const boost::optional<T>& opt) {
-    if (opt == boost::none) {
-      Py_RETURN_NONE;
-    }
-
-    return boost::python::incref(boost::python::object(*opt).ptr());
-  }
-
-  typedef boost::python::converter::rvalue_from_python_stage1_data py_s1_data;
-  typedef boost::python::converter::rvalue_from_python_storage<boost::optional<T>> py_storage;
-
-  static void register_from_python() {
-    boost::python::converter::registry::push_back(is_convertible,
-                                                  construct,
-                                                  boost::python::type_id<boost::optional<T>>());
-  }
-
-  static void* is_convertible(PyObject *obj) {
-    return (obj == Py_None || PyString_Check(obj)) ? obj : nullptr;
-  }
-
-  static void construct(PyObject *obj, py_s1_data *data) {
-    using namespace boost::python::converter;
-
-    void *raw = static_cast<void *>(reinterpret_cast<py_storage *>(data)->storage.bytes);
-    if (obj == Py_None) {
-      new (raw) boost::optional<T>{};
-    } else {
-      std::string value(PyString_AsString(obj));
-      std::cout << value << std::endl;
-      new (raw) boost::optional<T>(value);
-    }
-    data->convertible = raw;
-  }
-};
-
 static void
 nix_data_array_label_setter(nix::DataArray &da, const boost::optional<std::string> &lbl)
 {
@@ -85,6 +35,7 @@ nix_data_array_label_setter(nix::DataArray &da, const boost::optional<std::strin
 BOOST_PYTHON_MODULE(core)
 {
   using namespace boost::python;
+  using namespace nixpy;
 
   class_<nix::File>("File")
     .add_property("version", &nix::File::version)
