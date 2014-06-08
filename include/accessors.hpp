f485c7e5a58b8ec31206a74961a994e1438ff9f7
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2014-06-08 13:21:15 +0200

    accessor.hpp: Add macro for single entity getters
    Macro creates a getter that returns an boost::optional instead of
    an entity. Is used in order to get None in python instead of an
    uninitialized entity.

--- include/accessors.hpp
+++ include/accessors.hpp
@@ -18,6 +18,15 @@
 
 #define OPT_GETTER(type, class, name) static_cast<boost::optional<type>(class::*)()const>(&class::name)
 
+#define DEF_ENT_GETTER(type, class, name, getter_name) \
+    static boost::optional<type> getter_name(const class& obj) { \
+         type val = obj.name(); \
+         if (val) \
+             return boost::optional<type>(val); \
+         else \
+             return boost::none; \
+    }
+
 #define DEF_OPT_SETTER(type, class, name, setter_name) \
     static void setter_name(class& obj, const boost::optional<type>& val) { \
         if (val == boost::none) \
