14804fee18fa624e3678009ef60c49cde78c8489
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2014-06-24 09:01:46 +0200

    Copyright: finally arrived in 2014 :-)

--- src/PySection.hpp
+++ src/PySection.hpp
@@ -5,18 +5,3 @@
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted under the terms of the BSD License. See
 // LICENSE file in the root of the Project.
-
-#ifndef NIXPY_FILE_H
-#define NIXPY_FILE_H
-
-namespace nixpy {
-
-struct PyFile {
-
-    static void do_export();
-
-};
-
-}
-
-#endif
