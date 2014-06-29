ac22fd8af0fd41acfa5da398414ae6a16f535b7a
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2014-06-29 19:03:01 +0200

    PyValue: constructors and setter and setter

--- src/PyValue.cpp
+++ src/PyValue.cpp
@@ -21,13 +21,94 @@ using namespace boost::python;
 
 namespace nixpy {
 
+
+std::shared_ptr<Value> create(PyObject* value) {
+
+    std::shared_ptr<Value> created = std::make_shared<Value>(new Value());
+
+    if (PyBool_Check(value)) {
+        bool conv = extract<bool>(value);
+        created->set(conv);
+    } else if (PyInt_Check(value)) {
+        int64_t conv = extract<int64_t>(value);
+        created->set(conv);
+    } else if (PyFloat_Check(value)) {
+        double conv = extract<double>(value);
+        created->set(conv);
+    } else if (PyString_Check(value)) {
+        std::string conv = extract<std::string>(value);
+        created->set(conv);
+    } else {
+        throw std::runtime_error("Wrong type");
+    }
+
+    return created;
+}
+
+void set(Value& ref, PyObject* value) {
+
+    if (PyBool_Check(value)) {
+        bool conv = extract<bool>(value);
+        ref.set(conv);
+    } else if (PyInt_Check(value)) {
+        int64_t conv = extract<int64_t>(value);
+        ref.set(conv);
+    } else if (PyFloat_Check(value)) {
+        double conv = extract<double>(value);
+        ref.set(conv);
+    } else if (PyString_Check(value)) {
+        std::string conv = extract<std::string>(value);
+        ref.set(conv);
+    } else {
+        throw std::runtime_error("Wrong type");
+    }
+
+}
+
+PyObject* get(const Value& ref) {
+
+    DataType type = ref.type();
+    switch(type) {
+        case DataType::Bool:
+            return incref(object(ref.get<bool>()).ptr());
+        case DataType::Float:
+        case DataType::Double:
+            return incref(object(ref.get<double>()).ptr());
+        case DataType::Char:
+        case DataType::Int8:
+        case DataType::Int16:
+        case DataType::Int32:
+        case DataType::Int64:
+            return incref(object(ref.get<int64_t>()).ptr());
+        case DataType::UInt8:
+        case DataType::UInt16:
+        case DataType::UInt32:
+        case DataType::UInt64:
+            return incref(object(ref.get<u_int64_t>()).ptr());
+        case DataType::String:
+            return incref(object(ref.get<std::string>()).ptr());
+        case DataType::Date:
+        case DataType::DateTime:
+            // TODO support for date
+        case DataType::Nothing:
+        default:
+            Py_RETURN_NONE;
+    }
+}
+
 void PyValue::do_export() {
 
     class_<Value>("Value")
+        .def("__init__", make_constructor(create))
         .def_readwrite("reference", &Value::reference)
         .def_readwrite("filename", &Value::filename)
         .def_readwrite("encoder", &Value::encoder)
         .def_readwrite("checksum", &Value::checksum)
+        .def("set", set)
+        .def("get", get)
+        // Other
+        .def("__str__", &toStr<Value>)
+        .def("__repr__", &toStr<Value>)
         ;
 
     to_python_converter<std::vector<Value>, vector_transmogrify<Value>>();
