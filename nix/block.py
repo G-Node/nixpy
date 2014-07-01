a78fbc120bb032d14296330824e5e01923a116e6
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2014-07-01 11:32:07 +0200

    block.py: copyright header

--- nix/block.py
+++ nix/block.py
@@ -1,3 +1,11 @@
+# Copyright (c) 2014, German Neuroinformatics Node (G-Node)
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted under the terms of the BSD License. See
+# LICENSE file in the root of the Project.
+
 from __future__ import absolute_import
 
 from nix.core import Block
