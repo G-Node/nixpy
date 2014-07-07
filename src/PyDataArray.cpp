5e77569be86f3d3e94bf58557ac653820a35829a
Christian Kellner <christian@kellner.me>
2014-07-07 12:48:51 +0200

    PyDataArray: extract common array check code to make_array

--- src/PyDataArray.cpp
+++ src/PyDataArray.cpp
@@ -126,22 +126,27 @@ static nix::DataType py_dtype_to_nix_dtype(const PyArray_Descr *dtype)
     return nix::DataType::Nothing;
 }
 
-
-static void readData(DataArray& da, PyObject *data) {
-
+static PyArrayObject *make_array(PyObject *data, int requirements) {
     if (! PyArray_Check(data)) {
         throw std::invalid_argument("Data not a NumPy array");
     }
 
     PyArrayObject *array = reinterpret_cast<PyArrayObject *>(data);
 
+     if (! PyArray_CHKFLAGS(array, requirements)) {
+        throw std::invalid_argument("data must be c-contiguous and aligned");
+    }
+
+     return array;
+}
+
+static void readData(DataArray& da, PyObject *data) {
+
+    PyArrayObject *array = make_array(data, NPY_ARRAY_CARRAY);
+
     nix::DataType nix_dtype =  py_dtype_to_nix_dtype(PyArray_DESCR(array));
     if (nix_dtype == nix::DataType::Nothing) {
         throw std::invalid_argument("Unsupported dtype for data");
-    }
-
-    if (! PyArray_CHKFLAGS(array, NPY_ARRAY_CARRAY)) {
-        throw std::invalid_argument("data must be c-contiguous and aligned");
     }
 
     int array_rank = PyArray_NDIM(array);
@@ -159,19 +164,12 @@ static void readData(DataArray& da, PyObject *data) {
 
 
 static void writeData(DataArray& da, PyObject *data) {
-    if (! PyArray_Check(data)) {
-        throw std::invalid_argument("Data not a NumPy array");
-    }
 
-    PyArrayObject *array = reinterpret_cast<PyArrayObject *>(data);
+    PyArrayObject *array = make_array(data, NPY_ARRAY_CARRAY_RO);
 
     nix::DataType nix_dtype =  py_dtype_to_nix_dtype(PyArray_DESCR(array));
     if (nix_dtype == nix::DataType::Nothing) {
         throw std::invalid_argument("Unsupported dtype for data");
-    }
-
-    if (! PyArray_CHKFLAGS(array, NPY_ARRAY_CARRAY_RO)) {
-        throw std::invalid_argument("data must be c-contiguous and aligned");
     }
 
     int array_rank = PyArray_NDIM(array);
