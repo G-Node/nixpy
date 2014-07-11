7ccfd6d33b8df9164bd3c4e854a306ec5534183c
Christian Kellner <christian@kellner.me>
2014-07-11 15:48:54 +0200

    DataSet: Add support for sliced indexing (reading data)

--- nix/data_array.py
+++ nix/data_array.py
@@ -126,16 +126,24 @@ class DataSet(object):
         return raw
 
     def __getitem__(self, index):
-        if type(index) == int:
-            count = self.shape[1:]
-            raw = np.empty(count)
-            offset =  tuple(0 for _ in count)
-            self.__obj._read_data(raw, (1,) + count, (index, ) + offset)
-            return raw
-        elif index is Ellipsis:
+        if index is Ellipsis:
             return np.array(self)
+
+        tidx = type(index)
+        if tidx == tuple:
+            if len(index) < 1:
+                return np.array(self)
+        elif tidx == int or tidx == slice:
+            index = (index, )
         else:
             raise IndexError("Unsupported index")
+
+        # if we got to here we have a tuple with len >= 1
+        count, offset, shape = self.__tuple_to_count_offset_shape(index)
+
+        raw = np.empty(shape)
+        self.__obj._read_data(raw, count, offset)
+        return raw
 
     @property
     def shape(self):
@@ -156,3 +164,58 @@ class DataSet(object):
 
     def read_direct(self, data):
         self.__obj._read_data(data, (), ())
+
+    @staticmethod
+    def __complete_slices(shape, index):
+        if type(index) is slice:
+            if index.step is not None:
+                raise IndexError('Invalid index, stepping unsupported')
+            if index.start is None:
+                index = slice(0, index.stop)
+            if index.stop is None:
+                index = slice(index.start, shape)
+        elif type(index) is int:
+            index = slice(index, index+1)
+        elif index is None:
+            index = slice(0, shape)
+        else:
+            raise IndexError('Invalid index')
+        return index
+
+    @staticmethod
+    def __fill_none(shape, index):
+        size = len(shape) - len(index) + 1
+        return tuple([None] * size)
+
+    def __tuple_to_count_offset_shape(self, index):
+        # precondition: type(index) == tuple and len(index) >= 1
+        fill_none = self.__fill_none
+        shape = self.shape
+
+        if index[0] is Ellipsis:
+            index = fill_none(shape, index) + index[1:]
+        if index[-1] is Ellipsis:
+            # if we have a trailing ellipsis we just cut it away
+            # and let complete_slices do the right thing
+            index = index[:-1]
+
+        # here we handle Ellipsis in the middle of the tuple
+        # we *can* only handle one, if there are more, then
+        # __complete_slices will raise a InvalidIndex error
+        pos = index.index(Ellipsis) if Ellipsis in index else -1
+        if pos > -1:
+            index = index[:pos] + fill_none(shape, index) + index[pos+1:]
+
+        completed = map(self.__complete_slices, shape, index)
+        combined = map(lambda s: (s.start, s.stop), completed)
+        count = tuple(x[1] - x[0] for x in combined)
+        offset = zip(*combined)[0]
+
+        # drop all indices from count that came from single ints
+        # NB: special case when we only have ints, e.g. (int, ) then
+        # we get back the empty tuple and this is what we want,
+        # because it indicates a scalar result
+        squeezed = map(lambda i, c: c if type(i) != int else None, index, count)
+        shape = filter(lambda x: x is not None, squeezed)
+
+        return count, offset, shape
