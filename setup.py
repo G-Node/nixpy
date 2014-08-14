d72c04ad9c34eb53d8b9215ca1b08e23a75e7e47
Balint Morvai <balint@morvai.de>
2014-08-14 12:45:58 +0200

    Renamed files in setup.py

--- setup.py
+++ setup.py
@@ -73,8 +73,8 @@ nixpy_sources = [
     'src/PyDataArray.cpp',
     'src/PyDimensions.cpp',
     'src/PyFeature.cpp',
-    'src/PySimpleTag.cpp',
-    'src/PyDataTag.cpp',
+    'src/PyTag.cpp',
+    'src/PyMultiTag.cpp',
 ]
 
 boost_inc_dir = os.getenv('BOOST_INCDIR', '/usr/local/include')
