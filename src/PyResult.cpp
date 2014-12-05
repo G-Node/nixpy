c675f84b1777168db53fe6c9c5240fa87777b0e8
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2014-12-05 15:44:11 +0100

    PyResult: docstrings

--- src/PyResult.cpp
+++ src/PyResult.cpp
@@ -26,16 +26,18 @@ void PyResult::do_export() {
 
     class_<valid::Message>("Message",
                            init<std::string, std::string>())
-        .def_readonly("id", &valid::Message::id)
-        .def_readonly("msg", &valid::Message::msg);
+        .def_readonly("id", &valid::Message::id, doc::mesage_id)
+        .def_readonly("msg", &valid::Message::msg, doc::message_msg);
 
     class_<valid::Result>("Result",
                           init<std::vector<valid::Message>, std::vector<valid::Message>>())
-        .add_property("errors", &valid::Result::getErrors)
-        .add_property("warnings", &valid::Result::getWarnings)
-        .def("is_ok", &valid::Result::ok)
-        .def("has_errors", &valid::Result::hasErrors)
-        .def("has_warnings", &valid::Result::hasWarnings);
+        .add_property("errors", &valid::Result::getErrors,
+                      doc::result_errors)
+        .add_property("warnings", &valid::Result::getWarnings,
+                      doc::result_warnings)
+        .def("is_ok", &valid::Result::ok, doc::result_is_ok)
+        .def("has_errors", &valid::Result::hasErrors, doc::result_has_errors)
+        .def("has_warnings", &valid::Result::hasWarnings, doc::result_has_warnings);
 
     to_python_converter<std::vector<valid::Message>, vector_transmogrify<valid::Message>>();
     vector_transmogrify<valid::Message>::register_from_python();
