ac432465012b728fbc3c4ed25e35005a6116b110
Jan Grewe <jan.grewe@g-node.org>
2015-01-23 18:25:13 +0100

    add transforms for all exceptions

--- src/PyExceptions.cpp
+++ src/PyExceptions.cpp
@@ -44,9 +44,81 @@ static void translateDuplicateName(const nix::DuplicateName &e) {
     }
 }
 
+static void translateInvalidName(const nix::InvalidName &e) {
+    if (emptyMessage(e.what())) {
+        PyErr_SetString(PyExc_NameError, "Invalid name given - names have to be sanitized using util function.");
+    } else {
+        PyErr_SetString(PyExc_NameError, e.what());
+    }
+}
+
+static void translateEmptyString(const nix::EmptyString &e) {
+    if (emptyMessage(e.what())) {
+        PyErr_SetString(PyExc_NameError, "Empty string given - not a valid value for this field.");
+    } else {
+        PyErr_SetString(PyExc_NameError, e.what());
+    }
+}
+
+static void translateMissingAttr(const nix::MissingAttr &e) {
+    if (emptyMessage(e.what())) {
+        PyErr_SetString(PyExc_AttributeError, "Obligatory attribute is not set!");
+    } else {
+        PyErr_SetString(PyExc_AttributeError, e.what());
+    }
+}
+
+static void translateUninitializedEntity(const nix::UninitializedEntity &e) {
+    if (emptyMessage(e.what())) {
+        PyErr_SetString(PyExc_RuntimeError, "The Entity being accessed is uninitialized.");
+    } else {
+        PyErr_SetString(PyExc_RuntimeError, e.what());
+    }
+}
+
+static void translateUnsortedTicks(const nix::UnsortedTicks &e) {
+    if (emptyMessage(e.what())) {
+        PyErr_SetString(PyExc_ValueError, "Ticks are not given in a ascending order.");
+    } else {
+        PyErr_SetString(PyExc_ValueError, e.what());
+    }
+}
+
+static void translateIncompatibleDimensions(const nix::IncompatibleDimensions &e) {
+    if (emptyMessage(e.what())) {
+        PyErr_SetString(PyExc_ValueError, "The dimension descriptor is not compatible with the one stored int the dataArray!");
+    } else {
+        PyErr_SetString(PyExc_ValueError, e.what());
+    }
+}
+
+static void translateInvalidDimension(const nix::InvalidDimension &e) {
+    if (emptyMessage(e.what())) {
+        PyErr_SetString(PyExc_ValueError, "The provided dimension descriptor is invalid in this context!");
+    } else {
+        PyErr_SetString(PyExc_ValueError, e.what());
+    }
+}
+
+static void translateInvalidRank(const nix::InvalidRank &e) {
+    if (emptyMessage(e.what())) {
+        PyErr_SetString(PyExc_IndexError, "Invalid rank!");
+    } else {
+        PyErr_SetString(PyExc_IndexError, e.what());
+    }
+}
+
 void PyException::do_export() {
     register_exception_translator<nix::OutOfBounds>(&translateOutOfBounds);
     register_exception_translator<nix::DuplicateName>(&translateDuplicateName);
+    register_exception_translator<nix::InvalidName>(&translateInvalidName);
+    register_exception_translator<nix::EmptyString>(&translateEmptyString);
+    register_exception_translator<nix::InvalidRank>(&translateInvalidRank);
+    register_exception_translator<nix::InvalidDimension>(&translateInvalidDimension);
+    register_exception_translator<nix::IncompatibleDimensions>(&translateIncompatibleDimensions);
+    register_exception_translator<nix::UnsortedTicks>(&translateUnsortedTicks);
+    register_exception_translator<nix::UninitializedEntity>(&translateUninitializedEntity);
+    register_exception_translator<nix::MissingAttr>(&translateMissingAttr);
 }
 
 }
