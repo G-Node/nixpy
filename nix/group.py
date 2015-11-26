683df3ce9751cccb40fb5531d531e40c18b8543a
Jan Grewe <jan.grewe@g-node.org>
2015-11-26 18:34:01 +0100

    tag support in groups

--- nix/group.py
+++ nix/group.py
@@ -10,8 +10,27 @@ from __future__ import (absolute_import, division, print_function, unicode_liter
 
 from nix.core import Group
 from nix.util.inject import inject
-from nix.util.proxy_list import ProxyList, RefProxyList
-from nix.block import DataArrayProxyList
+from nix.util.proxy_list import RefProxyList
+
+
+class DataArrayProxyList(RefProxyList):
+
+    def __init__(self, obj):
+        super(DataArrayProxyList, self).__init__(
+            obj, "_data_array_count", "_get_data_array_by_id",
+            "_get_data_array_by_pos", "_delete_data_array_by_id",
+            "_add_data_array_by_id"
+        )
+
+
+class TagProxyList(RefProxyList):
+
+    def __init__(self, obj):
+        super(TagProxyList, self).__init__(
+            obj, "_tag_count", "_get_tag_by_id",
+            "_get_tag_by_pos", "_delete_tag_by_id",
+            "_add_tag_by_id"
+        )
 
 
 class GroupMixin(Group):
@@ -19,30 +38,32 @@ class GroupMixin(Group):
     @property
     def data_arrays(self):
         """
-        A property containing all data arrays referenced by the tag. Referenced data arrays
+        A property containing all data arrays referenced by the group. Referenced data arrays
         can be obtained by index or their id. References can be removed from the list, removing
         a referenced DataArray will not remove it from the file. New references can be added using
         the append method of the list.
         This is a read only attribute.
 
-        :type: RefProxyList of DataArray
+        :type: DataArrayProxyList of DataArray
         """
         if not hasattr(self, "_data_arrays"):
             setattr(self, "_data_arrays", DataArrayProxyList(self))
         return self._data_arrays
-    """
-    @property
-    def features(self):
 
-        A property containing all features of the tag. Features can be
-        obtained via their index or their id. Features can be deleted from the list.
-        Adding new features to the tag is done using the create_feature method.
+    @property
+    def tags(self):
+        """
+        A property containing all tags referenced by the group. Tags
+        can be obtained by index or their id. Tags can be removed from the list, removing
+        a referenced Tag will not remove it from the file. New Tags can be added using
+        the append method of the list.
         This is a read only attribute.
 
-        :type: ProxyList of Feature.
-                if not hasattr(self, "_features"):
-            setattr(self, "_features", FeatureProxyList(self))
-        return self._features
-    """
+        :type: TagProxyList of Tags
+        """
+        if not hasattr(self, "_tags"):
+            setattr(self, "_tags", TagProxyList(self))
+        return self._tags
+
 
 inject((Group,), dict(GroupMixin.__dict__))
--- src/PyGroup.cpp
+++ src/PyGroup.cpp
@@ -33,6 +33,19 @@ boost::optional<DataArray> getDataArrayByPos(const Group& g, size_t index) {
     return da ? boost::optional<DataArray>(da) : boost::none;
 }
 
+// getter for Tags
+boost::optional<Tag> getTagById(const Group& g, const std::string& id) {
+    Tag t = g.getTag(id);
+
+    return t? boost::optional<Tag>(t) : boost::none;
+}
+
+boost::optional<Tag> getTagByPos(const Group& g, size_t index) {
+    Tag t = g.getTag(index);
+
+    return t ? boost::optional<Tag>(t) : boost::none;
+}
+
 void PyGroup::do_export() {
 
     PyEntityWithSources<base::IGroup>::do_export("Group");
@@ -46,7 +59,14 @@ void PyGroup::do_export() {
         .def("_get_data_array_by_id", &getDataArrayById)
         .def("_get_data_array_by_pos", &getDataArrayByPos)
         .def("_delete_data_array_by_id", REMOVER(std::string, Group, removeDataArray))
-        // Features
+        // Tags
+        .def("_add_tag_by_id", REF_SETTER(std::string, Group, addTag))
+        .def("_has_tag_by_id", CHECKER(std::string, Group, hasTag))
+        .def("_tag_count", &Group::tagCount)
+        .def("_get_tag_by_id", &getTagById)
+        .def("_get_tag_by_pos", &getTagByPos)
+        .def("_delete_tag_by_id", REMOVER(std::string, Group, removeTag))
+
         /*
         .def("create_feature", &createNewFeature, doc::tag_create_feature)
         .def("_has_feature_by_id", CHECKER(std::string, Tag, hasFeature))
