0bb7555e9a1753bf9815d71729cf07abe8b2d6e1
Prabhjot Singh <prabhjot.nith@gmail.com>
2016-03-09 20:10:35 +0530

    final file according to PEP8

--- docs/source/examples/multipleROIs.py
+++ docs/source/examples/multipleROIs.py
@@ -13,8 +13,8 @@
 
  This tutorial shows how to store image data in nix-files.
  See https://github.com/G-node/nix/wiki for more information.
- 
- We use the "Lenna" image in this tutorial.  
+
+ We use the "Lenna" image in this tutorial.
 
  "Lenna" by Original full portrait: "Playmate of the Month". Playboy
  Magazine. November 1972, photographed by Dwight Hooker.This 512x512
@@ -29,6 +29,7 @@ import numpy as np
 import Image as img
 import matplotlib.pyplot as plt
 
+
 def load_image():
     image = img.open('lenna.png')
     pix = np.array(image)
@@ -38,7 +39,7 @@ def load_image():
 
 def draw_rect(img_data, position, extent):
     img_data[position[0]:position[0] + extent[0], position[1], :] = 255
-    img_data[position[0]:position[0] + extent[0], position[1]+extent[1], :] = 255
+    img_data[position[0]:position[0] + extent[0], position[1] + extent[1], :] = 255
     img_data[position[0], position[1]:position[1] + extent[1], :] = 255
     img_data[position[0] + extent[0], position[1]:position[1] + extent[1], :] = 255
     return img_data
@@ -51,18 +52,19 @@ def plot_data(tag):
     positions_data = tag.positions[:]
     extents_data = tag.extents[:]
     for i in range(positions.data_extent[0]):
-        img_data = draw_rect(img_data, positions_data[i,:], extents_data[i,:])
+        img_data = draw_rect(
+            img_data, positions_data[i, :], extents_data[i, :])
     new_img = img.fromarray(img_data)
     new_img.show()
 
 
 def plot_roi_data(tag):
-    position_count = tag.positions.shape[0] 
+    position_count = tag.positions.shape[0]
     for p in range(position_count):
         roi_data = tag.retrieve_data(p, 0)[:]
         roi_data = np.array(roi_data, dtype='uint8')
-        ax = plt.gcf().add_subplot(position_count,1,p)
-        image = img.fromarray(roi_data)  
+        ax = plt.gcf().add_subplot(position_count, 1, p)
+        image = img.fromarray(roi_data)
         ax.imshow(image)
     plt.savefig('retrieved_rois.png')
     plt.show()
@@ -75,10 +77,11 @@ if __name__ == '__main__':
     file = nix.File.open(file_name, nix.FileMode.Overwrite)
 
     # create a 'Block' that represents a grouping object. Here, the recording session.
-    # it gets a name and a type 
+    # it gets a name and a type
     block = file.create_block("block name", "nix.session")
 
-    # create a 'DataArray' to take the sinewave, add some information about the signal
+    # create a 'DataArray' to take the sinewave, add some information about
+    # the signal
     data = block.create_data_array("lenna", "nix.image.rgb", data=img_data)
     # add descriptors for width, height and channels
     height_dim = data.append_sampled_dimension(1)
@@ -87,30 +90,33 @@ if __name__ == '__main__':
     width_dim.label = "width"
     color_dim = data.append_set_dimension()
     color_dim.labels = channels
- 
-    # some space for three regions-of-interest
-    roi_starts = np.zeros((3,3))
-    roi_starts[0, :] = [250, 245, 0] 
-    roi_starts[1, :] = [250, 315, 0] 
-    roi_starts[2, :] = [340, 260, 0] 
 
-    roi_extents = np.zeros((3,3))    
-    roi_extents[0, :] = [30, 45, 3] 
-    roi_extents[1, :] = [30, 40, 3] 
-    roi_extents[2, :] = [25, 65, 3] 
- 
+    # some space for three regions-of-interest
+    roi_starts = np.zeros((3, 3))
+    roi_starts[0, :] = [250, 245, 0]
+    roi_starts[1, :] = [250, 315, 0]
+    roi_starts[2, :] = [340, 260, 0]
+
+    roi_extents = np.zeros((3, 3))
+    roi_extents[0, :] = [30, 45, 3]
+    roi_extents[1, :] = [30, 40, 3]
+    roi_extents[2, :] = [25, 65, 3]
+
     # create the positions DataArray
-    positions = block.create_data_array("ROI positions", "nix.positions", data=roi_starts)
-    positions.append_set_dimension() # these can be empty
+    positions = block.create_data_array(
+        "ROI positions", "nix.positions", data=roi_starts)
+    positions.append_set_dimension()  # these can be empty
     positions.append_set_dimension()
- 
+
     # create the extents DataArray
-    extents = block.create_data_array("ROI extents", "nix.extents", data=roi_extents)
+    extents = block.create_data_array(
+        "ROI extents", "nix.extents", data=roi_extents)
     extents.append_set_dimension()
     extents.append_set_dimension()
 
     # create a MultiTag
-    multi_tag = block.create_multi_tag("Regions of interest", "nix.roi", positions)
+    multi_tag = block.create_multi_tag(
+        "Regions of interest", "nix.roi", positions)
     multi_tag.extents = extents
     multi_tag.references.append(data)
 
