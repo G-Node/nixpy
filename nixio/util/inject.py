1f256c274c1d20c098613a94c35f98ba28c4d926
Achilleas Koutsou <achilleas.k@gmail.com>
2016-05-20 12:09:17 +0300

    Exclude __doc__ when patching classes

--- nixio/util/inject.py
+++ nixio/util/inject.py
@@ -1,7 +1,7 @@
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 
 
-excludes = ("__module__", "__metaclass__", "__dict__")
+excludes = ("__module__", "__metaclass__", "__dict__", "__doc__")
 
 """ 
 Does monkey patching to the classes in 'bases' by adding
