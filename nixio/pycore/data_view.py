b458a4fa31dcb9ccdfde4fec114f00194e1c5330
Achilleas Koutsou <achilleas.k@gmail.com>
2016-07-25 13:55:49 +0200

    DataView count and offset are now private properties

--- nixio/pycore/data_view.py
+++ nixio/pycore/data_view.py
@@ -16,10 +16,10 @@ class DataView(DataSet):
     def __init__(self, da, count, offset):
         self.array = da
         self._h5group = self.array._h5group
-        self.count = tuple(count)
-        self.offset = tuple(offset)
+        self._count = tuple(count)
+        self._offset = tuple(offset)
 
-        co = tuple(c + o for c, o in zip(self.count, self.offset))
+        co = tuple(c + o for c, o in zip(self._count, self._offset))
         if any(c > e for c, e in zip(co, self.array.data_extent)):
             raise OutOfBounds(
                 "Trying to create DataView which is out of bounds"
@@ -27,7 +27,7 @@ class DataView(DataSet):
 
     @property
     def data_extent(self):
-        return self.count
+        return self._count
 
     @property
     def data_type(self):
@@ -35,23 +35,23 @@ class DataView(DataSet):
 
     def _write_data(self, data, count, offset):
         if not count:
-            count = self.count
+            count = self._count
         offset = self._transform_coordinates(count, offset)
         return super(DataView, self)._write_data(data, count, offset)
 
     def _read_data(self, data, count, offset):
         if not count:
-            count = self.count
+            count = self._count
         offset = self._transform_coordinates(count, offset)
         return super(DataView, self)._read_data(data, count, offset)
 
     def _transform_coordinates(self, count, offset):
         if not offset:
-            if np.any(np.greater(count, self.count)):
+            if np.any(np.greater(count, self._count)):
                 raise OutOfBounds("Trying to access data outside of range")
-            return self.offset
+            return self._offset
         else:
             co = tuple(c + o for c, o in zip(count, offset))
-            if any(c > sc for c, sc in zip(co, self.count)):
+            if any(c > sc for c, sc in zip(co, self._count)):
                 raise OutOfBounds("Trying to access data outside of range")
-            return tuple(so + o for so, o in zip(self.offset, offset))
+            return tuple(so + o for so, o in zip(self._offset, offset))
