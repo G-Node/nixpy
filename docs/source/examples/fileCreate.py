a24114b848426b470140b83f816ec2f6fcefc16a
Achilleas Koutsou <achilleas.k@gmail.com>
2016-08-10 14:11:38 +0200

    Leftover nix->nixio changes in example files

--- docs/source/examples/fileCreate.py
+++ docs/source/examples/fileCreate.py
@@ -3,20 +3,20 @@
 
 from __future__ import print_function
 
-import nixio as nix
+import nixio
 
 
 file_name = 'file_create_example.h5'
 
 # create a new file overwriting any existing content
-file = nix.File.open(file_name, nix.FileMode.Overwrite)
+file = nixio.File.open(file_name, nixio.FileMode.Overwrite)
 print(file.format, file.version, file.created_at)
 
 # close file
 file.close()
 
 # re-open file for read-only access
-file  = nix.File.open(file_name, nix.FileMode.ReadOnly)
+file  = nixio.File.open(file_name, nixio.FileMode.ReadOnly)
 
 # this command will fail putting out HDF5 Errors
 file.create_block("test block", "test")
@@ -24,7 +24,7 @@ file.create_block("test block", "test")
 file.close()
 
 # re-open for read-write access
-file  = nix.File.open(file_name, nix.FileMode.ReadWrite)
+file = nixio.File.open(file_name, nixio.FileMode.ReadWrite)
 
 # the following command now works fine
 file.create_block("test block", "test")
