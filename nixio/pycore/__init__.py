c7f8da5e9a0cd5f4bcc1cdf4bfeb840d8caa8be5
Achilleas Koutsou <achilleas.k@gmail.com>
2017-04-13 16:23:43 +0200

    [Block,Source] Remove Mixins and move outside Pycore

--- nixio/pycore/__init__.py
+++ nixio/pycore/__init__.py
@@ -1,10 +1,8 @@
 from .property import Property
 from .section import Section
-from .source import Source
 from .data_array import DataArray
 from .dimensions import RangeDimension, SetDimension, SampledDimension
 from .feature import Feature
 from .tag import Tag
 from .multi_tag import MultiTag
 from .group import Group
-from .block import Block
