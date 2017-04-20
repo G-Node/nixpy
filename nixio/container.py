41acc1005e228ac210c0bb1ba41f4e582a7d58f6
Achilleas Koutsou <achilleas.k@gmail.com>
2017-04-20 18:53:56 +0200

    [Group] Using LinkContainer instead of RefProxyList
    LinkContainer requires reference to parent Block containers.
    Container and LinkContainer __init__ docstrings added.
    LinkContainer reimplements certain functions due to difference in
    indexing links in the backend.

--- nixio/container.py
+++ nixio/container.py
@@ -13,14 +13,16 @@ class Container(object):
     Container acts as an interface to container groups in the backend. In the
     case of HDF5, this is a group that is used as a container for other groups.
 
-    Most methods of this class are simple interfaces to the respective methods
-    of the backend class (e.g., H5Group).
-
-    Examples of containers:
+    Examples of Containers:
         Block.data_arrays
         Block.tags
         Block.multi_tags
         Source.sources
+
+    :param name: Name of the container
+    :param parent: Parent H5Group where this container will be created
+    :param itemclass: The class of the objects this container holds (for
+    checking and instantiations)
     """
 
     def __init__(self, name, parent, itemclass):
@@ -39,7 +41,7 @@ class Container(object):
             if item < 0:
                 item = len(self) + item
             if item < 0 or item >= len(self):
-                raise IndexError("Index out of bounds: {} ({})".format(item))
+                raise IndexError("Index out of bounds: {}".format(item))
             item = self._backend.get_by_pos(item)
         else:
             item = self._backend.get_by_id_or_name(item)
@@ -47,7 +49,9 @@ class Container(object):
 
     def __delitem__(self, item):
         if isinstance(item, int):
-            item = self._backend.get_by_pos(item).name
+            item = self._inst_item(self._backend.get_by_pos(item))
+        if isinstance(item, self._itemclass):
+            item = self._item_key(item)
         self._backend.delete(item)
 
     def __iter__(self):
@@ -55,14 +59,16 @@ class Container(object):
             yield self._inst_item(group)
 
     def __contains__(self, item):
-        if isinstance(item, strtype):
+        if isinstance(item, self._itemclass):
+            return item.name in self._backend
+        if util.is_uuid(item):
             try:
-                self[item]
+                self._backend.get_by_id(item)
                 return True
             except ValueError:
                 return False
         else:
-            return item.name in self._backend
+            return item in self._backend
 
     def __str__(self):
         return "[{}]".format(
@@ -72,31 +78,48 @@ class Container(object):
     def __repr__(self):
         return str(self)
 
+    @staticmethod
+    def _item_key(item):
+        return item.name
+
 
 class LinkContainer(Container):
     """
-    LinkContainer is a counterpart to the Container class for linked objects.
-    Link containers differ from regular containers in the following ways:
-    - Linked objects are not created by the parent object but are appended to
-    the LinkContainer explicitly. Therefore, LinkContainer provides append()
-    and extend() methods.
-    - Linked objects are indexed by id rather than name.
+    A LinkContainer acts as an interface to container groups in the backend
+    that hold links to objects already contained in a Container.
 
-    Most methods of this class are simple interfaces to the respective methods
-    of the backend class (e.g., H5Group).
+    Objects are added to a LinkContainer using the 'append' method, as
+    opposed to Containers which get populated by the parent object 'create'
+    methods.
+
+    An important difference between a LinkContainer and a Container is that
+    links to objects are indexed by their 'id' whereas objects in Containers
+    are indexed by 'name'.
 
     Examples of LinkContainers:
         Group.data_arrays
         Group.tags
+        Group.multi_tags
+
+    :param name: Name of the container
+    :param parent: Parent H5Group where this container will be created
+    :param itemclass: The class of the objects this container holds (for
+    checking and instantiations)
+    :param itemstore: The location (Container) where the original objects
+    are stored and linked to.
     """
-    def __init__(self, name, parent):
-        self._backend = parent.open_group(name)
+    def __init__(self, name, parent, itemclass, itemstore):
+        super(LinkContainer, self).__init__(name, parent, itemclass)
+        self._itemstore = itemstore
 
     def append(self, item):
         if util.is_uuid(item):
-            item = self._backend.get_by_id(item)
+            item = self._inst_item(self._backend.get_by_id(item))
 
-        if not hasattr(item, "id"):
+        if item not in self._itemstore:
+            raise RuntimeError("This item cannot be appended here.")
+
+        if hasattr(item, "id"):
             self._backend.create_link(item, item.id)
         else:
             raise TypeError("NIX entity or id string required for append")
@@ -106,3 +129,32 @@ class LinkContainer(Container):
             raise TypeError("{} object is not iterable".format(type(items)))
         for item in items:
             self.append(item)
+
+    def __getitem__(self, item):
+        if isinstance(item, int):
+            super(LinkContainer, self).__getitem__(item)
+        else:
+            if util.is_uuid(item):
+                # name is key for LinkContainer
+                item = self._backend.get_by_name(item)
+            else:
+                for grp in self._backend:
+                    if item == grp.get_attr("name"):
+                        item = grp
+                        break
+        return self._inst_item(item)
+
+    def __contains__(self, item):
+        # need to redefine because of id indexing/linking
+        if isinstance(item, self._itemclass):
+            return item.id in self._backend
+        if util.is_uuid(item):
+            return item in self._backend
+        for grp in self._backend:
+            if item == grp.get_attr("name"):
+                return True
+        return False
+
+    @staticmethod
+    def _item_key(item):
+        return item.id
