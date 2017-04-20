2c14a3b64b55d279d954dbc07f2d306ef36e3552
Achilleas Koutsou <achilleas.k@gmail.com>
2017-04-20 16:23:00 +0200

    [File] Delete old, obsolete ProxyList interfaces

--- nixio/file.py
+++ nixio/file.py
@@ -286,18 +286,6 @@ class File(object):
         block = Block._create_new(self, self._data, name, type_)
         return block
 
-    def _get_block_by_id(self, id_or_name):
-        return Block(self, self._data.get_by_id_or_name(id_or_name))
-
-    def _get_block_by_pos(self, pos):
-        return Block(self, self._data.get_by_pos(pos))
-
-    def _delete_block_by_id(self, id_):
-        self._data.delete(id_)
-
-    def _block_count(self):
-        return len(self._data)
-
     # Section
     def create_section(self, name, type_):
         """
@@ -315,18 +303,6 @@ class File(object):
             raise exceptions.DuplicateName("create_section")
         sec = Section._create_new(self, self.metadata, name, type_)
         return sec
-
-    def _get_section_by_id(self, id_or_name):
-        return Section(self, self.metadata.get_by_id_or_name(id_or_name))
-
-    def _get_section_by_pos(self, pos):
-        return Section(self, self.metadata.get_by_pos(pos))
-
-    def _delete_section_by_id(self, id_):
-        self.metadata.delete(id_)
-
-    def _section_count(self):
-        return len(self.metadata)
 
     @property
     def blocks(self):
