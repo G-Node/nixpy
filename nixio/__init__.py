2a4ac1a2e26f8f89c1b93eb199db98c3c2aa720f
Achilleas Koutsou <achilleas.k@gmail.com>
2017-06-09 17:21:42 +0200

    Import entity classes to top level namespace

--- nixio/__init__.py
+++ nixio/__init__.py
@@ -6,6 +6,16 @@
 # modification, are permitted under the terms of the BSD License. See
 # LICENSE file in the root of the Project.
 from .file import File, FileMode
+from .block import Block
+from .group import Group
+from .data_array import DataArray
+from .tag import Tag
+from .multi_tag import MultiTag
+from .section import Section
+from .property import Property
+from .dimensions import RangeDimension, SampledDimension, SetDimension
+from .feature import Feature
+
 from .value import Value, DataType
 from .dimension_type import DimensionType
 from .link_type import LinkType
