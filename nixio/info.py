8670d1479f5bbd3328f3ad6429721ddc47a1035f
Achilleas Koutsou <achilleas.k@gmail.com>
2018-02-21 18:02:31 +0100

    [info] Information in json file
    - Package info in json file inside project source root
    - info.py loads json file and populates variables
    - info.json is packaged with sources

--- nixio/info.py
+++ nixio/info.py
@@ -5,13 +5,20 @@
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted under the terms of the BSD License. See
 # LICENSE file in the root of the Project.
+import os
+import json
 
-VERSION = '1.4.dev'
-STATUS = 'Release'
-RELEASE = '%s %s' % (VERSION, STATUS)
-AUTHOR = 'Christian Kellner, Adrian Stoewer, Andrey Sobolev, Jan Grewe,\
-Balint Morvai, Achilleas Koutsou'
-COPYRIGHT = '2014, German Neuroinformatics Node, ' + AUTHOR
-CONTACT = 'dev@g-node.org'
-BRIEF = 'Python bindings for NIX'
-HOMEPAGE = 'https://github.com/G-Node/nixpy'
+here = os.path.dirname(__file__)
+
+with open(os.path.join(here, "info.json")) as infofile:
+    infodict = json.load(infofile)
+
+
+VERSION = infodict["VERSION"]
+STATUS = infodict["STATUS"]
+RELEASE = infodict["RELEASE"]
+AUTHOR = infodict["AUTHOR"]
+COPYRIGHT = infodict["COPYRIGHT"]
+CONTACT = infodict["CONTACT"]
+BRIEF = infodict["BRIEF"]
+HOMEPAGE = infodict["HOMEPAGE"]
