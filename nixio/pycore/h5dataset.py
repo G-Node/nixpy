1d5108a9551e46fe1a281d345fb33520f28bb39e
Achilleas Koutsou <achilleas.k@gmail.com>
2018-05-14 16:02:19 +0200

    [h5dataset] Fix for indexing and slicing in h5py backend
    Fixes #296

--- nixio/pycore/h5dataset.py
+++ nixio/pycore/h5dataset.py
@@ -50,11 +50,10 @@ class H5DataSet(object):
                 return data
             datashape = data.shape
             sl = util.co_to_slice(count, offset)
-            if (isinstance(sl, tuple) and
-                    np.ndim(data) != len(sl) and
-                    count[-1] == 1):
-                    # data needs an extra dimension of size 1 to match slice
-                    data.resize(datashape + (1,))
+            if isinstance(sl, tuple) and np.ndim(data) < len(sl):
+                # data shape needs dimension padding to match dataset
+                # diimensionality
+                data.resize(count)
             self.dataset.read_direct(data, sl)
             # if data was resized, it should be returned to its original shape
             data.resize(datashape)
