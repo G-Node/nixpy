fa3a623bfc28481c5a48e2181731e2cccb3ea074
M. Sonntag <michael.p.sonntag@gmail.com>
2018-07-13 16:07:44 +0200

    [property] Add DataType class

--- nixio/property.py
+++ nixio/property.py
@@ -10,10 +10,57 @@
 import numpy as np
 from collections import Sequence
 from enum import Enum
+from numbers import Number, Integral, Real
+from six import string_types
 
 from .entity import Entity
-from .value import Value, DataType
+from .value import Value
 from . import util
+
+
+bools = (bool, np.bool_)
+valid_types = (Number, string_types, bools)
+
+
+class DataType(object):
+    UInt8 = np.uint8
+    UInt16 = np.uint16
+    UInt32 = np.uint32
+    UInt64 = np.uint64
+    Int8 = np.int8
+    Int16 = np.int16
+    Int32 = np.int32
+    Int64 = np.int64
+    Float = np.float_
+    Double = np.double
+    String = np.string_
+    Bool = np.bool_
+
+    @classmethod
+    def get_dtype(cls, value):
+        if isinstance(value, bools):
+            return cls.Bool
+        elif isinstance(value, Integral):
+            return cls.Int64
+        elif isinstance(value, Real):
+            return cls.Float
+        elif isinstance(value, string_types):
+            return cls.String
+        else:
+            raise ValueError("Unknown type for value {}".format(value))
+
+    @classmethod
+    def is_numeric_dtype(cls, dtype):
+        return (dtype == cls.Int8 or
+                dtype == cls.Int16 or
+                dtype == cls.Int32 or
+                dtype == cls.Int64 or
+                dtype == cls.UInt8 or
+                dtype == cls.UInt16 or
+                dtype == cls.UInt32 or
+                dtype == cls.UInt64 or
+                dtype == cls.Float or
+                dtype == cls.Double)
 
 
 class OdmlType(str, Enum):
