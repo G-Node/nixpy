e99a6e3cc0840b8612708091ad29ab88fcc2b867
Achilleas Koutsou <achilleas.k@gmail.com>
2018-07-20 17:32:14 +0200

    [test] Bad appends and checks for container classes

--- nixio/test/test_container.py
+++ nixio/test/test_container.py
@@ -84,3 +84,103 @@ class TestContainer(unittest.TestCase):
         self.assertEqual(self.group.data_arrays[0]._parent, self.block)
         self.assertEqual(self.group.tags[0]._parent, self.block)
         self.assertEqual(self.group.multi_tags[0]._parent, self.block)
+
+    def test_bad_appends(self):
+        # use fresh file for this one
+        filename = os.path.join(self.tmpdir.path, "badappend.nix")
+        nf = nix.File.open(filename, nix.FileMode.Overwrite)
+
+        for blockname in ["a", "b"]:
+            block = nf.create_block(blockname, "block")
+
+            for groupname in ["a", "b"]:
+                group = block.create_group(blockname + groupname, "group")
+                for daname in ["a", "b"]:
+                    da = block.create_data_array(
+                        blockname + groupname + daname, "data", data=[0]
+                    )
+                    group.data_arrays.append(da)
+                    tag = block.create_tag(
+                        blockname + groupname + daname, "tag", position=[0]
+                    )
+                    group.tags.append(tag)
+
+        # check counts
+        self.assertEqual(len(nf.blocks), 2)
+        for bl in nf.blocks:
+            self.assertEqual(len(bl.groups), 2)
+            self.assertEqual(len(bl.data_arrays), 4)
+            self.assertEqual(len(bl.tags), 4)
+            for g in bl.groups:
+                self.assertEqual(len(g.data_arrays), 2)
+                self.assertEqual(len(g.tags), 2)
+
+        # cross-block append
+        with self.assertRaises(RuntimeError):
+            nf.blocks[0].groups[0].data_arrays.append(
+                nf.blocks[1].data_arrays[1]
+            )
+
+        # another
+        with self.assertRaises(RuntimeError):
+            nf.blocks[1].groups[0].tags.append(
+                nf.blocks[0].tags[1]
+            )
+
+        # append data array to group.tags
+        with self.assertRaises(TypeError):
+            nf.blocks[0].groups[0].tags.append(
+                nf.blocks[0].data_arrays[2]
+            )
+
+        # cross-block *and* incorrect type
+        with self.assertRaises(TypeError):
+            nf.blocks[0].groups[0].tags.append(
+                nf.blocks[1].data_arrays[2]
+            )
+
+        # let's do sources
+        for bl in nf.blocks:
+            for sourcename in ["a", "b"]:
+                src = bl.create_source(bl.name + sourcename, "source")
+                for chsourcename in ["a", "b", "c"]:
+                    src.create_source(bl.name + sourcename + chsourcename,
+                                      "source")
+
+        # check counts
+        for bl in nf.blocks:
+            self.assertEqual(len(bl.sources), 2)
+            for src in bl.sources:
+                self.assertEqual(len(src.sources), 3)
+
+        # cross-block source append
+        with self.assertRaises(RuntimeError):
+            nf.blocks[1].groups[0].sources.append(
+                nf.blocks[0].sources[1]
+            )
+
+        # cross-block source append (deep)
+        with self.assertRaises(RuntimeError):
+            nf.blocks[1].groups[0].sources.append(
+                nf.blocks[0].sources[1].sources[2]
+            )
+
+        # valid deep append
+        nf.blocks[0].groups[0].data_arrays[1].sources.append(
+            nf.blocks[0].sources[1].sources[2]
+        )
+
+        # bad membership test
+        with self.assertRaises(TypeError):
+            nf.blocks[1].data_arrays[1] in nf.blocks[0].tags
+
+        # another
+        with self.assertRaises(TypeError):
+            nf.blocks[1].data_arrays[1] in nf.blocks[0].groups[0]
+
+        # bad membership test on LinkContainer
+        with self.assertRaises(TypeError):
+            nf.blocks[1].data_arrays[1] in nf.blocks[0].groups[0].tags
+
+    def test_dangling_references(self):
+        pass
