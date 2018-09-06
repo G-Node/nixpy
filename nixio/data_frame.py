24a23942e406b1fb53a21550d0e4111ef33dc4d3
Chek Yin Choi <chekchek2007@gmail.com>
2018-09-06 18:21:16 +0200

    writing basic API

---
+++ nixio/data_frame.py
@@ -0,0 +1,18 @@
+from __future__ import (absolute_import, division, print_function)
+import numpy as np
+
+
+class DataFrame():
+
+    def __init__(self, base=None): # base is for defining what is the row - blks or dataarrays
+        pass
+
+    def check_valid(self):
+        pass # if the data is suitable for making dataframe
+
+    def setup(self):
+        pass # check number of arrays/ blocks and form frame
+
+    def columns(self):
+        pass 
+
