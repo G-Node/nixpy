6e52e4abf547c1430933a974cbeaf2cb17da8179
Jan Grewe <jan.grewe@g-node.org>
2018-10-25 10:23:03 +0200

    [pyFile] fix warning ...
    throw invalid_argument when wrong flag is passed to file open,
    should never occur anyway

--- src/PyFile.cpp
+++ src/PyFile.cpp
@@ -32,7 +32,7 @@ File open(std::string path, std::string mode = "a", std::string compression = "A
     } else if (mode == "r") {
         nixmode = FileMode::ReadOnly;
     } else {
-        // TODO: Raise error
+      throw std::invalid_argument("File::open: invalid file mode flag (valid flags are: a, w, r for read-write, overwrite and read-only, respectively)!");
     }
     return File::open(path, nixmode, "hdf5", pyCompressionToNix(compression));
 }
