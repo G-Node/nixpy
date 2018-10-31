ea789cc9856c3b3da5cdd928902db1eb05b903de
Chek Yin Choi <chekchek2007@gmail.com>
2018-10-31 14:11:58 +0100

    unit.setter support single unit change

--- nixio/data_frame.py
+++ nixio/data_frame.py
@@ -24,7 +24,7 @@ class DataFrame(Entity, DataSet):
         for nam, type in col_dict.items():
             if type == str:
                 col_dict[nam] = util.vlen_str_dtype
-        cls.name = name
+        cls.df_name = name
         cls.raw_shape = shape
         cls.col_names = np.array(list(col_dict.keys()))
         cls.dt_arr = list(col_dict.items())
@@ -48,7 +48,7 @@ class DataFrame(Entity, DataSet):
             li.append(new_col[i])
             tu = tuple(li)
             new_da.append(tu)
-        farr = np.ascontiguousarray(new_da, dtype=(self.col_dtype))
+        farr = np.ascontiguousarray(new_da, dtype=self.col_dtype)
         del self._h5group.group['data']
         self._h5group.group['data'] = farr
         self._h5group.create_dataset("data", (self.shape[0],), self.col_dtype)
@@ -63,20 +63,20 @@ class DataFrame(Entity, DataSet):
         self.append(pro_data, axis=0)
         self.raw_shape = tuple((self.raw_shape[0] + len(data), self.raw_shape[1]))
 
-    def write_column(self, changed_col, col_idx=None, column_name=None):  # Done
+    def write_column(self, changed_col, col_idx=None, column_name=None):
         if len(changed_col) != self.raw_shape[0]:
-            raise  ValueError('if missing data, please fill None')
+            raise ValueError('if missing data, please fill None')
         if not col_idx and not column_name:
             raise ValueError("Either index or name must not be None")
         if column_name is None:
-            column_name = self._find_name_by_idx(col_idx)  # find name by name
+            column_name = self._find_name_by_idx(col_idx)
         changed_col = np.array(changed_col)
         for i, rows in enumerate(self._h5group.group['data'][:]):
             cell = changed_col[i]
             rows[column_name] = cell
             self.write_rows(changed_row=rows, row_idx=[i])
 
-    def read_columns(self, col_idx= None, col_name=None):  # Done
+    def read_columns(self, col_idx=None, col_name=None):
         if col_idx is None and col_name is None:
             raise ValueError("Either index or name must not be None")
         if col_name is None:
@@ -87,7 +87,7 @@ class DataFrame(Entity, DataSet):
         get_col = self._read_data(sl=slice)[col_name]
         return get_col
 
-    def write_rows(self, changed_row, row_idx = None):  # Done!
+    def write_rows(self, changed_row, row_idx=None):
         if row_idx is None:
             raise ValueError("Index must be specified")
         x, y = self.raw_shape
@@ -99,32 +99,32 @@ class DataFrame(Entity, DataSet):
         else:
             cr_list = []
             for i, cr in enumerate(changed_row):
-               cr_list.append(tuple(cr))
-            print(cr_list)
-            print(type(cr_list))
+                cr_list.append(tuple(cr))
             self._write_data(cr_list,sl=row_idx)
 
-    def read_rows(self, row_idx):  # Done
+    def read_rows(self, row_idx):
         get_row = self._read_data(sl=(row_idx, ))
         return get_row
 
-    def write_cell(self, new_item, position= None, col_name=None, row_idx=None):  #Done
+    def write_cell(self, new_item, position = None, col_name=None, row_idx=None):
         if position:
-            if len(position) != 2: raise ValueError('not a position')
+            if len(position) != 2:
+                raise ValueError('not a position')
             x, y = position
-            targeted_row = self.read_row(x)
+            targeted_row = self.read_rows(x)
             targeted_row[y] = new_item
             self._write_data(targeted_row, sl=x)
         else:
             if col_name is None and row_idx is None:
                 raise ValueError("Column and rows identifier must be given")
-            targeted_row = self.read_row(row_idx)
+            targeted_row = self.read_rows(row_idx)
             targeted_row[col_name] = new_item
             self._write_data(targeted_row,sl=row_idx)
 
-    def read_cell(self, position= None, col_name=None, row_idx=None):  # Done
+    def read_cell(self, position= None, col_name=None, row_idx=None):
         if position:
-            if len(position) != 2: raise ValueError('Not a position')
+            if len(position) != 2:
+                raise ValueError('Not a position')
             x, y = position
             return self[x][y]
         else:
@@ -138,7 +138,7 @@ class DataFrame(Entity, DataSet):
         if self.unit:
             print(row_form.format("unit", *self.unit))
         for i, row in enumerate(self._h5group.group['data'][:]):
-            print(row_form.format("Data{}".format(i),*row))
+            print(row_form.format("Data{}".format(i), *row))
 
     def write_to_csv(self):
         with open('t.csv', 'w', newline='') as csvfile:
@@ -154,7 +154,7 @@ class DataFrame(Entity, DataSet):
             dw.writerows(di)
 
     def _find_idx_by_name(self, name):
-        for  i, n in enumerate(self.col_names):
+        for i, n in enumerate(self.col_names):
             if n == name:
                 return i
         return None
@@ -174,21 +174,30 @@ class DataFrame(Entity, DataSet):
         return self._h5group.get_attr("unit")
 
     @unit.setter
-    def unit(self, u):
-        if len(u) != len(self.col_dtype):
-            raise ValueError("Length mismatched")
-        self._h5group.set_attr("unit", u)
+    def unit(self, u, col_idx=None):
+        col_len = len(self.col_dtype)
+        if col_idx is None:
+            if len(u) != col_len:
+                raise ValueError("Please specify a column index or give units for all columns")
+            self._h5group.set_attr("unit", u)
+        else:
+            if self._h5group.get_attr("unit") is None:
+                u_list = [None] * col_len
+                u_list[col_idx] = u
+                self._h5group.set_attr("unit", u_list)
+            else:
+                u_list = self._h5group.get_attr("unit")
+                u_list[col_idx] = u
+                self._h5group.set_attr("unit", u_list)
 
     @property
     def data_type(self):
         return self._h5group.get_attr('dtype')
 
-    @data_type.setter
-    def data_type(self):
-        dt = self.col_dtype
-        self._h5group.set_attr('dtype', dt)
-
     @property
     def columns(self):
-        return self.col_names, self.dtype, self.unit
-
+        if self.unit:
+            cols = [(n, dt, u) for n, dt, u in zip(self.col_names, self.col_raw_dtype, self.unit)]
+        else:
+            cols = [(n, dt, None) for n, dt in zip(self.col_names, self.col_raw_dtype)]
+        return cols
