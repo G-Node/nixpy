0572c5541568a5d9edbd5dfb96e06d5829e11270
Achilleas Koutsou <achilleas.k@gmail.com>
2019-01-17 03:22:04 +0100

    Run compatibility tests only when requested
    The compatibility tests are no longer run by default when NIX is
    detected. They can be run by specifying --nix-compat. If specified, the
    tests are compiled and any errors will cause failure of the
    compatibility tests. If the option is not specified, the tests are
    skipped, even if NIX is available.
    
    One reason for this change is that the compatibility tests were always
    compiled when NIX was available, even when they are unselected, and
    compilation slows down the tests.
    
    With this change, they are only compiled and run when explicitly
    enabled.

--- conftest.py
+++ conftest.py
@@ -4,15 +4,12 @@ from nixio.test.xcompat.compile import maketests
 
 
 BINDIR = tempfile.mkdtemp(prefix="nixpy-tests-")
-xcavail = maketests(BINDIR)
 
 
 def pytest_addoption(parser):
-    parser.addoption("--force-compat", action="store_true", default=False,
-                     help=("Force run compatibility tests. "
-                           "If they fail to compile (e.g., missing NIX) "
-                           "the tests wont pass.")
-                     )
+    parser.addoption("--nix-compat", action="store_true", default=False,
+                     help=("Run nix compatibility tests "
+                           "(requires NIX library)"))
 
 
 @pytest.fixture
@@ -21,14 +18,13 @@ def bindir(request):
 
 
 def pytest_collection_modifyitems(config, items):
-    if config.getoption("--force-compat"):
-        print("Forcing compatibility tests")
+    if config.getoption("--nix-compat"):
+        print("Compiling NIX compatibility tests")
+        maketests(BINDIR)
         return
-    if not xcavail:
-        print("Skipping compatibility tests")
-        skip_compat = pytest.mark.skip(
-            reason="Compatibility tests require the NIX library"
-        )
-        for item in items:
-            if "compatibility" in item.keywords:
-                item.add_marker(skip_compat)
+    skip_compat = pytest.mark.skip(
+        reason="Use --nix-compat option to run compatibility tests"
+    )
+    for item in items:
+        if "compatibility" in item.keywords:
+            item.add_marker(skip_compat)
