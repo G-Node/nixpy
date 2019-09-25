e40169c53a81aaf649fccd361868c72167bd25a2
Achilleas Koutsou <achilleas.k@gmail.com>
2019-09-25 15:47:47 +0200

    DataView write test: More cases for indexing

--- nixio/test/test_data_view.py
+++ nixio/test/test_data_view.py
@@ -46,33 +46,62 @@ class TestDataView(unittest.TestCase):
         np.testing.assert_almost_equal(da[:], newdata)
 
     def test_data_view_write_index(self):
+        """
+        Write through DataView to the underlying DataArray using [slice]
+        notation.
+
+        Update copy of underlying data with expected values and compare
+        directly to DataArray for assertions.
+        """
         da = self.file.blocks[0].data_arrays[0]
 
         dv = da.get_slice((10, 20), extents=(20, 15))
+        newdata = self.data.copy()
+
+        # straightforward slicing
         ow = np.random.random((3, 4))
         dv[0:3, 1:5] = ow
-
-        newdata = self.data.copy()
         newdata[10:13, 21:25] = ow
-
         np.testing.assert_almost_equal(da[:], newdata)
 
+        # slicing with steps
         ow = np.random.random((5, 3))
         dv[0:20:4, 0:15:5] = ow
         newdata[10:30:4, 20:35:5] = ow
         np.testing.assert_almost_equal(da[:], newdata)
 
+        # steps only
         ow = np.random.random((5, 3))
         dv[::4, ::5] = ow
         newdata[10:30:4, 20:35:5] = ow
         np.testing.assert_almost_equal(da[:], newdata)
 
+        # plain index
         dv[1, 1] = 20
         newdata[11, 21] = 20
         np.testing.assert_almost_equal(da[:], newdata)
 
+        # negative index
         dv[-1, -1] = 80
         newdata[29, 34] = 80
+        np.testing.assert_almost_equal(da[:], newdata)
+
+        # negative slice start
+        ow = np.random.random(3)
+        dv[-3::, 10] = ow
+        newdata[27:30, 30] = ow
+        np.testing.assert_almost_equal(da[:], newdata)
+
+        # negative slice stop
+        ow = np.random.random(15)
+        dv[:-5, 2] = ow
+        newdata[10:25, 22] = ow
+        np.testing.assert_almost_equal(da[:], newdata)
+
+        # negative slice start and stop
+        ow = np.random.random(10)
+        dv[10, -15:-5] = ow
+        newdata[20, 20:30] = ow
         np.testing.assert_almost_equal(da[:], newdata)
 
     def test_data_view_oob_write(self):
