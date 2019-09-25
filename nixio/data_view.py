9e0fa3bc8321a8ce5b70f87f952aaa7294f1d106
Achilleas Koutsou <achilleas.k@gmail.com>
2019-09-25 13:31:20 +0200

    [DataView] Constructor checks and slice conversion
    - Dimension count check (on top of bounds checks) in DataView
    constructor: DataView dimensionality must match array dims exactly.
    - Simplify incoming slices using the 'slice.indices()' function:
    Converts potential negative or None 'start' or 'stop' to positive
    equivalents.
    - Pluralise '_slices' attribute to represent that it's a collection
    (tuple) of slice objects.

--- nixio/data_view.py
+++ nixio/data_view.py
@@ -12,24 +12,38 @@ try:
 except ImportError:
     from collections import Iterable
 from .data_set import DataSet
-from .exceptions import OutOfBounds
+from .exceptions import OutOfBounds, IncompatibleDimensions
 
 
 class DataView(DataSet):
 
-    def __init__(self, da, sl):
+    def __init__(self, da, slices):
+        if len(slices) != len(da.shape):
+            # This is always checked by the calling function, but we repeat
+            # the check here for future bug catching
+            raise IncompatibleDimensions(
+                "Number of dimensions for DataView does not match underlying "
+                "DataArray: {} != {}".format(len(slices), len(da.shape)),
+                "DataView"
+            )
+
+        if any(s.stop > e for s, e in zip(slices, da.data_extent)):
+            raise OutOfBounds(
+                "Trying to create DataView which is out of bounds of the "
+                "underlying DataArray"
+            )
+
+        # Simplify all slices
+        slices = tuple(slice(*sl.indices(dimlen))
+                       for sl, dimlen in zip(slices, da.shape))
+
         self.array = da
         self._h5group = self.array._h5group
-        self._slice = sl
-
-        if any(s.stop > e for s, e in zip(sl, self.array.data_extent)):
-            raise OutOfBounds(
-                "Trying to create DataView which is out of bounds"
-            )
+        self._slices = slices
 
     @property
     def data_extent(self):
-        return tuple(s.stop - s.start for s in self._slice)
+        return tuple(s.stop - s.start for s in self._slices)
 
     @data_extent.setter
     def data_extent(self, v):
@@ -39,14 +53,18 @@ class DataView(DataSet):
     def data_type(self):
         return self.array.data_type
 
-    def _write_data(self, data, count, offset):
-        if not count:
-            count = self._count
-        offset = self._transform_coordinates(count, offset)
-        return super(DataView, self)._write_data(data, count, offset)
+    def _write_data(self, data, sl=None):
+        tsl = self._slices
+        if sl:
+            tsl = self._transform_coordinates(sl)
+        super(DataView, self)._write_data(data, tsl)
 
     def _read_data(self, sl=None):
-        dvslices = self._slice
+        # tsl = self._slices
+        # if sl:
+        #     tsl = self._transform_coordinates(sl)
+        # return super(DataView, self)._read_data(tsl)
+        dvslices = self._slices
         # complete DataView slices (turn Nones into values)
         dvslices = tuple(slice(*dv.indices(l)) for dv, l in
                          zip(dvslices, self.array.shape))
