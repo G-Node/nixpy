369e459d35c03167a6048993ec33626603bb4fe4
Achilleas Koutsou <achilleas.k@gmail.com>
2019-10-10 19:43:33 +0200

    [validator] Test MultiTags
    Disable failing tests until they are rewritten

--- nixio/test/test_validator.py
+++ nixio/test/test_validator.py
@@ -24,7 +24,8 @@ class TestValidate (unittest.TestCase):
                                          "validation-test.data_array",
                                          dtype=nix.DataType.Float,
                                          data=np.random.random(5))
-            da1d.append_range_dimension(ticks=[0.1, 0.2, 1.5, 2.4, 3.0])
+            rdim = da1d.append_range_dimension(ticks=[0.1, 0.2, 1.5, 2.4, 3.0])
+            rdim.unit = "ms"
 
             da2d = blk.create_data_array("data-2d",
                                          "validation-test.data_array",
@@ -58,9 +59,9 @@ class TestValidate (unittest.TestCase):
             mtag1d = blk.create_multi_tag(
                 "mtag1d", "validation-test.multi_tag", positions=pos1d
             )
-            mtag1d.references.append(blk.data_arrays["data-1d"])
+            mtag1d.references.append(da1d)
+            mtag1d.units = ("s",)
 
-            # TODO: Multi-dimensional multitag
             pos2d = blk.create_data_array(
                 "pos-2d", "validation-test.multi_tag.positions",
                 data=[(10, 2, 100), (20, 5, 101)]
@@ -78,6 +79,8 @@ class TestValidate (unittest.TestCase):
                 "mtag2d", "validation-test.multi_tag", positions=pos2d
             )
             mtag2d.extents = ext2d
+            mtag2d.references.append(da3d)
+            mtag2d.units = ("", "", "us")
 
             # TODO: Add Features
 
@@ -212,9 +215,9 @@ class TestValidate (unittest.TestCase):
 
     def test_check_tag_mismatch_units(self):
         err_unit_len = ("some of the referenced DataArrays' dimensions "
-                        "don't have units where the tag has; "
+                        "don't have units where the Tag has; "
                         "make sure that all references have the same number "
-                        "of dimensions as the tag has units "
+                        "of dimensions as the Tag has units "
                         "and that each dimension has a unit set")
         tag = self.file.blocks[0].tags[0]
         tag.units = ("V", "A")
@@ -241,98 +244,75 @@ class TestValidate (unittest.TestCase):
         tagerr = res["errors"][tag]
         assert errmsg in tagerr
 
-    def test_check_multi_tag(self):
-        mt1 = self.block1.multi_tags[0]
-        da1 = self.block1.create_data_array(name='test1', array_type='t',
-                                            dtype='float', data=0)
-        mt1.extents = da1
-        self.validator.check_multi_tag(mt1, 0, 0)
-        assert self.validator.errors['blocks'][0]['multi_tags'][0]['errors']\
-            == ['Number of entries in positions and extents do not match']
+    def test_check_multi_tag_no_pos(self):
+        blk = self.file.blocks[0]
+        mtag = blk.multi_tags["mtag1d"]
+        mtag.positions = blk.create_data_array("empty", "bork", data=[])
+        res = self.file.validate()
+        mtagerr = res["errors"][mtag]
+        # will also report mismatch in dimensions with reference
+        assert "positions are not set" in mtagerr
 
-        # test for pos & extent with multiple dimensions
-        mt2 = self.block1.multi_tags[1]
-        da2 = self.block1.create_data_array(name='test2a', array_type='t',
-                                            dtype='float',
-                                            data=np.random.random((6, 5)))
-        da3 = self.block1.create_data_array(name='test2b', array_type='t',
-                                            dtype='float',
-                                            data=np.random.random((5, 8)))
-        da4 = self.block1.create_data_array(name='test2c', array_type='t',
-                                            dtype='float',
-                                            data=np.random.random((5, 4)))
-        da5 = self.block1.create_data_array(name='test2d', array_type='t',
-                                            dtype='float',
-                                            data=np.random.random((5, 3)))
-        mt2.references.append(da2)
-        mt2.references.append(da3)
-        mt2.positions = da5
-        mt2.extents = da4
-        self.validator.check_multi_tag(mt2, 1, 0)
-        warn1 = "The number of reference and position entries do not match"
-        warn2 = "The number of reference and extent entries do not match"
-        # pos-extent-mismatch warning exist but not assert
-        # change shape of da4/5 to elim warning
-        mterr = self.validator.errors['blocks'][0]['multi_tags'][1]['errors']
-        assert warn1 in mterr
-        assert warn2 in mterr
+    def test_check_multi_tag_mismatch_dim(self):
+        errmsg = ("number of entries (in 2nd dim) in positions does not match "
+                  "number of dimensions in all referenced DataArrays")
+        blk = self.file.blocks[0]
+        mtag = blk.multi_tags["mtag2d"]
+        mtag.positions = blk.create_data_array("wrong dim", "bork",
+                                               data=[(1, 1), (2, 2)])
+        res = self.file.validate()
+        mtagerr = res["errors"][mtag]
+        assert errmsg in mtagerr
 
-        # test for pos & extent with only ONE dimensions
-        mt3 = self.block1.multi_tags[2]
-        da6 = self.block1.create_data_array(name='test3a', array_type='t',
-                                            dtype='float',
-                                            data=np.random.random(5))
-        da7 = self.block1.create_data_array(name='test3b', array_type='t',
-                                            dtype='float',
-                                            data=np.random.random((3, 3)))
-        mt3.positions = da6
-        mt3.extents = da6
-        mt3.references.append(da7)
-        self.validator.check_multi_tag(mt3, 2, 0)
-        mterr = self.validator.errors['blocks'][0]['multi_tags'][2]['errors']
-        assert warn1 in mterr
-        assert warn2 in mterr
+    def test_check_multi_tag_invalid_unit(self):
+        errmsg = ("unit is invalid: not an atomic SI "
+                  "(Note: composite units are not supported)")
+        blk = self.file.blocks[0]
+        mtag = blk.multi_tags["mtag2d"]
+        mtag.units = ['abc']
+        res = self.file.validate()
+        tagerr = res["errors"][mtag]
+        assert errmsg in tagerr
 
-        # test for ext and pos dimensionality
-        mt4 = self.block1.multi_tags[3]
-        da8 = self.block1.create_data_array(name='test3c', array_type='t',
-                                            dtype='float',
-                                            data=np.random.random((5, 3, 4)))
-        mt4.positions = da8
-        mt4.extents = da8
-        self.validator.check_multi_tag(mt4, 3, 0)
-        assert "Positions should not have more than 2 dimensions" \
-               in self.validator.errors['blocks'][0]['multi_tags'][3]['errors']
-        assert "Extents should not have more than 2 dimensions" \
-               in self.validator.errors['blocks'][0]['multi_tags'][3]['errors']
+    def test_check_multi_tag_mismatch_units(self):
+        err_unit_len = ("some of the referenced DataArrays' dimensions "
+                        "don't have units where the MultiTag has; "
+                        "make sure that all references have the same number "
+                        "of dimensions as the MultiTag has units "
+                        "and that each dimension has a unit set")
+        mtag = self.file.blocks[0].multi_tags["mtag2d"]
+        mtag.units = ("V", "A")
+        res = self.file.validate()
+        tagerr = res["errors"][mtag]
+        assert err_unit_len in tagerr
 
-        # test for units
-        mt5 = self.block2.multi_tags[0]
-        mt5.units = ['s', 'abc']
-        da9 = self.block2.data_arrays[0]
-        mt5.references.append(da9)
-        da9.append_range_dimension([1, 2, 3, 4, 5, 6, 7, 8, 9])
-        da9.dimensions[0]._h5group.set_attr("unit", "mV")
-        self.validator.check_multi_tag(mt5, 0, 1)
-        mtagerr = self.validator.errors['blocks'][1]['multi_tags'][0]['errors']
-        assert "Invalid unit" in mtagerr
-        assert "References and multi_tag units mismatched" in mtagerr
+        err_unit_match = ("some of the referenced DataArrays' dimensions "
+                          "have units that are not convertible to the units "
+                          "set in the MultiTag "
+                          "(Note: composite units are not supported)")
+        mtag.units = ("V", "A", "L")
+        res = self.file.validate()
+        tagerr = res["errors"][mtag]
+        assert err_unit_match in tagerr
 
-        # 2nd test for units
-        da9.dimensions[0]._h5group.set_attr("unit", "ms")
-        mt5.units = ['s']
-        self.validator.check_multi_tag(mt5, 0, 1)
-        mtagerr = self.validator.errors['blocks'][1]['multi_tags'][0]['errors']
-        assert mtagerr == []
+    def test_check_multi_tag_pos_ext_mismatch(self):
+        errmsg = ("number of entries in extent does not match "
+                  "number of dimensions in all referenced DataArrays")
+        tag = self.file.blocks[0].tags[0]
+        tag.extent = [100]
 
-    def test_check_section(self):  # only have check for basics now
+        res = self.file.validate()
+        tagerr = res["errors"][tag]
+        assert errmsg in tagerr
+
+    def _test_check_section(self):  # only have check for basics now
         sec1 = self.file.sections[0]
         sec1._h5group.set_attr("type", None)
         self.validator.check_section(sec1, 0)
         err = self.validator.errors['sections'][0]['errors']
         assert "Type of Section is missing" in err
 
-    def test_check_props(self):
+    def _test_check_props(self):
         # check1
         section = self.file.sections[0]
         prop = section.create_property("prop1", [1, 2, 3, 4])
@@ -356,10 +336,10 @@ class TestValidate (unittest.TestCase):
         err = self.validator.errors['sections'][0]['props'][2]['errors']
         assert 'Unit is not valid!' in err
 
-    def test_check_features(self):
+    def _test_check_features(self):
         pass  # RuntimeError will be raised, so no need for test
 
-    def test_range_dim(self):
+    def _test_range_dim(self):
         err_dict = self.validator.errors['blocks'][0]['data_arrays']
         # check1
         da1 = self.block1.data_arrays[0]
@@ -381,7 +361,7 @@ class TestValidate (unittest.TestCase):
         assert "Ticks need to be set for range dimensions" in err
         assert "Unit must be atomic, not composite!" in err
 
-    def test_sample_dim(self):
+    def _test_sample_dim(self):
         # check1
         da1 = self.block1.data_arrays[0]
         sdim1 = da1.append_sampled_dimension(sampling_interval=0.5)
@@ -403,7 +383,7 @@ class TestValidate (unittest.TestCase):
         assert "Unit must be atomic, not composite!" in err
         assert "SamplingInterval is not set to valid value (> 0)!" in err
 
-    def test_set_dim(self):
+    def _test_set_dim(self):
         da1 = self.block1.data_arrays[0]
         setdim1 = da1.append_set_dimension()
         setdim1._h5group.set_attr('dimension_type', 'range')
@@ -413,7 +393,7 @@ class TestValidate (unittest.TestCase):
         dimerr = da['dimensions'][0]['errors']
         assert "Dimension type is not correct!" in dimerr
 
-    def test_sources(self):
+    def _test_sources(self):
         da1 = self.block1.data_arrays[0]
         src1 = self.block1.create_source("src1", "testing_src")
         da1.sources.append(src1)
