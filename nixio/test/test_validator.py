20d72eb6c2ab7a099c8944e1b3b6c4c36703b6db
Achilleas Koutsou <achilleas.k@gmail.com>
2019-10-10 17:53:03 +0200

    [validator] Test rewrite
    Create fully valid file in test setUp() and create one error or warning
    per test.

--- nixio/test/test_validator.py
+++ nixio/test/test_validator.py
@@ -10,44 +10,97 @@ from .tmp import TempDir
 class TestValidate (unittest.TestCase):
 
     def setUp(self):
+        # Create completely valid objects and break for each test
         self.tmpdir = TempDir("validatetest")
         self.testfilename = os.path.join(self.tmpdir.path, "validatetest.nix")
         self.file = nix.File.open(self.testfilename, nix.FileMode.Overwrite)
-        self.block1 = self.file.create_block("blk1", "blk")
-        self.block2 = self.file.create_block("blk2", "blk2")
-        for blk in self.block1, self.block2:
-            for i in range(2):
-                blk.create_group("grp{}".format(i), "groups")
-            for i in range(4):
-                blk.create_data_array("da{}".format(i), "data_arrays",
-                                      dtype="float",
-                                      data=(1 + np.random.random(5)))
-            for i in range(4):
-                blk.create_tag("tag{}".format(i), "tags",
-                               np.random.random(1))
-            for i in range(4):
-                blk.create_multi_tag("mt{}".format(i), "multi_tags",
-                                     blk.data_arrays[i])
+        for bidx in range(2):
+            blk = self.file.create_block("block-{}".format(bidx),
+                                         "validation-test.block")
+            for gidx in range(2):
+                blk.create_group("group-{}".format(gidx),
+                                 "validation-test.group")
+            da1d = blk.create_data_array("data-1d",
+                                         "validation-test.data_array",
+                                         dtype=nix.DataType.Float,
+                                         data=np.random.random(5))
+            da1d.append_range_dimension(ticks=[0.1, 0.2, 1.5, 2.4, 3.0])
 
-        self.file.create_section("sec1", "test")
+            da2d = blk.create_data_array("data-2d",
+                                         "validation-test.data_array",
+                                         dtype=nix.DataType.Float,
+                                         data=np.random.random((2, 10)))
+            setdim = da2d.append_set_dimension()
+            setdim.labels = ("A", "B")
+            da2d.append_sampled_dimension(0.1)
+
+            da3d = blk.create_data_array("data-3d",
+                                         "validation-test.data_array",
+                                         dtype=nix.DataType.Float,
+                                         data=np.random.random((4, 2, 10)))
+            setdim = da3d.append_set_dimension()
+            setdim.labels = ("1", "2", "3", "4")
+            da3d.append_set_dimension()  # set dim without labels
+            smpldim = da3d.append_sampled_dimension(0.05)
+            smpldim.unit = "s"
+
+            tag = blk.create_tag("tag", "validation-test.tag",
+                                 position=(1, 1, 1))
+            tag.extent = (2, 1, 5)
+            tag.references.append(blk.data_arrays["data-3d"])
+            tag.units = ("", "", "ms")
+
+            pos1d = blk.create_data_array(
+                "pos-1d", "validation-test.multi_tag.positions",
+                data=[0, 1, 10, 20]
+            )
+            pos1d.append_set_dimension()
+            mtag1d = blk.create_multi_tag(
+                "mtag1d", "validation-test.multi_tag", positions=pos1d
+            )
+            mtag1d.references.append(blk.data_arrays["data-1d"])
+
+            # TODO: Multi-dimensional multitag
+            pos2d = blk.create_data_array(
+                "pos-2d", "validation-test.multi_tag.positions",
+                data=[(10, 2, 100), (20, 5, 101)]
+            )
+            pos2d.append_set_dimension()
+            pos2d.append_set_dimension()
+
+            ext2d = blk.create_data_array(
+                "ext-3d", "validation-test.multi_tag.extents",
+                data=[(1, 1, 1), (5, 5, 5)]
+            )
+            ext2d.append_set_dimension()
+            ext2d.append_set_dimension()
+            mtag2d = blk.create_multi_tag(
+                "mtag2d", "validation-test.multi_tag", positions=pos2d
+            )
+            mtag2d.extents = ext2d
+
+            # TODO: Add Features
+
+            # TODO: Add sources
+
+        # TODO: Add Sections and Properties
 
     def tearDown(self):
         self.file.close()
         self.tmpdir.cleanup()
 
-    def test_check_file(self):
+    def test_all_valid(self):
         res = self.file.validate()
-        assert self.file not in res["errors"]
-        assert self.file not in res["warnings"]
+        assert not res["errors"], self.print_all_results(res)
+        assert not res["warnings"], self.print_all_results(res)
+
+    def test_check_file(self):
         self.file.force_created_at(0)
         res = self.file.validate()
         assert res["errors"][self.file] == ["date is not set"]
 
-    def test_check_blocks(self):
-        res = self.file.validate()
-        block = self.block1
-        assert block not in res["errors"]
-
+    def test_check_block(self):
+        block = self.file.blocks[0]
         block._h5group.set_attr("name", None)
         res = self.file.validate()
         assert res["errors"][block] == ["no name set"]
@@ -59,9 +112,9 @@ class TestValidate (unittest.TestCase):
         assert actual == expected
         assert len(res["warnings"]) == 0
 
-    def test_check_groups(self):
-        group1 = self.block1.groups[0]
-        group2 = self.block2.groups[1]
+    def test_check_group(self):
+        group1 = self.file.blocks[0].groups[0]
+        group2 = self.file.blocks[1].groups[0]
         group1._h5group.set_attr("name", None)
         res = self.file.validate()
         assert res["errors"][group1] == ["no name set"]
@@ -81,106 +134,112 @@ class TestValidate (unittest.TestCase):
         assert actual == expected
         assert len(res["warnings"]) == 0
 
-    def test_check_data_arrays(self):
-        expected_errors = list()
-        expected_warnings = list()
-        somedata = np.random.randint(10, size=(5, 5))
-        da1 = self.block1.create_data_array("data", "type",
-                                            dtype=nix.DataType.Int32,
-                                            data=somedata)
-        da1.append_range_dimension([1, 2, 3, 4, 5, 6, 7, 8, 9])
-        expected_errors.append(
-            "number of ticks in RangeDimension (1) differs from the number "
+    def test_check_data_array_wrong_ticks(self):
+        expmsg = ("number of ticks in RangeDimension (1) differs from the "
+                  "number of data entries along the corresponding "
+                  "data dimension")
+        da = self.file.blocks[0].data_arrays["data-1d"]
+        rdim = da.dimensions[0]
+        rdim.ticks = [10]
+        res = self.file.validate()
+        assert res["errors"][da] == [expmsg]
+
+    def test_check_data_array_wrong_labels(self):
+        expmsg = (
+            "number of labels in SetDimension (1) differs from the number "
             "of data entries along the corresponding data dimension"
         )
-        setdim = da1.append_set_dimension()
-        setdim.labels = ["A", "B", "C", "D"]
-        expected_errors.append(
-            "number of labels in SetDimension (2) differs from the number "
-            "of data entries along the corresponding data dimension"
-        )
-        da1._h5group.set_attr("type", None)
-        expected_errors.append("no type set")
-        da1._h5group.set_attr("expansion_origin", 0.11)  # poly not set
-        expected_warnings.append(
+        da = self.file.blocks[0].data_arrays["data-2d"]
+        setdim = da.dimensions[0]
+        setdim.labels = ["-"]
+        res = self.file.validate()
+        assert res["errors"][da] == [expmsg]
+
+    def test_check_data_array_coefficients(self):
+        no_polynom_coeff_warning = (
             "expansion origin for calibration is set, "
             "but polynomial coefficients are missing"
         )
+        no_expansion_origin_warning = (
+            "polynomial coefficients for calibration are set, "
+            "but expansion origin is missing"
+        )
+        da1 = self.file.blocks[0].data_arrays["data-1d"]
+        da1.expansion_origin = 0.7
+
+        da2 = self.file.blocks[1].data_arrays["data-1d"]
+        da2.polynom_coefficients = [2, 4]
 
         res = self.file.validate()
+        warnings = res["warnings"]
+        assert warnings[da1] == [no_polynom_coeff_warning]
+        assert warnings[da2] == [no_expansion_origin_warning]
 
-        da_err = res["errors"][da1]
-        da_warn = res["warnings"][da1]
-
-        assert sorted(da_err) == sorted(expected_errors)
-        assert sorted(da_warn) == sorted(expected_warnings)
-
-        da2 = self.block1.data_arrays[1]
-        setdim = da2.append_set_dimension()
-        setdim.labels = ["A", "B", "C", "D", "E"]
-        da2.polynom_coefficients = [0.1, 0.2]
+    def test_check_data_array_bad_dims(self):
+        expmsg = (
+            "data dimensionality does not match number of defined dimensions"
+        )
+        da = self.file.blocks[1].data_arrays["data-3d"]
+        da.append_set_dimension()
         res = self.file.validate()
-        da_warn = res["warnings"][da2]
-        assert da_warn == ["polynomial coefficients for calibration are set, "
-                           "but expansion origin is missing"]
+        assert res["errors"][da] == [expmsg]
 
-        da3 = self.block1.data_arrays[0]
+    def test_check_tag_no_pos(self):
+        tag = self.file.blocks[0].tags[0]
+        tag.position = []
         res = self.file.validate()
-        da_err = res["errors"][da3]
-        assert da_err == ["data dimensionality does not match number of "
-                          "defined dimensions"]
+        tagerr = res["errors"][tag]
+        # will also report mismatch in dimensions with reference
+        assert "position is not set" in tagerr
 
-    def test_check_tags(self):
-        tag1 = self.block1.tags[0]
-        tag1.position = []
-        self.validator.check_tag(tag1, 0, 0)
-        tagerr = self.validator.errors['blocks'][0]['tags'][0]['errors']
-        assert tagerr == ['Position is not set!']
+    def test_check_tag_mismatch_dim(self):
+        errmsg = ("number of entries in position does not match "
+                  "number of dimensions in all referenced DataArrays")
+        tag = self.file.blocks[0].tags[0]
+        tag.position = [4, 3, 2, 1]
+        res = self.file.validate()
+        tagerr = res["errors"][tag]
+        assert errmsg in tagerr
 
-        tag2 = self.block1.tags[1]
-        tag2.units = ['abc']
-        self.validator.check_tag(tag2, 1, 0)
-        tagerr = self.validator.errors['blocks'][0]['tags'][1]['errors']
-        assert tagerr == ['Invalid unit']
+    def test_check_tag_invalid_unit(self):
+        errmsg = ("unit is invalid: not an atomic SI "
+                  "(Note: composite units are not supported)")
+        tag = self.file.blocks[0].tags[0]
+        tag.units = ['abc']
+        res = self.file.validate()
+        tagerr = res["errors"][tag]
+        assert errmsg in tagerr
 
-        tag3 = self.block1.tags[2]
-        da1 = self.block1.data_arrays[0]
-        da1.append_range_dimension([1, 2, 3, 4, 5, 6, 7, 8, 9])
-        da1.dimensions[0]._h5group.set_attr("unit", "s")
-        da1.append_sampled_dimension(0.5)
-        da1.dimensions[1]._h5group.set_attr("unit", "A")
-        tag3.references.append(da1)
-        tag3.units = ['A', 'mV']
-        self.validator.check_tag(tag3, 2, 0)
-        assert self.validator.errors['blocks'][0]['tags'][2]['errors'] == [
-            'References and tag units mismatched']
+    def test_check_tag_mismatch_units(self):
+        err_unit_len = ("some of the referenced DataArrays' dimensions "
+                        "don't have units where the tag has; "
+                        "make sure that all references have the same number "
+                        "of dimensions as the tag has units "
+                        "and that each dimension has a unit set")
+        tag = self.file.blocks[0].tags[0]
+        tag.units = ("V", "A")
+        res = self.file.validate()
+        tagerr = res["errors"][tag]
+        assert err_unit_len in tagerr
 
-        tag4 = self.block1.tags[3]
-        da2 = self.block1.data_arrays[1]
-        da3 = self.block1.data_arrays[2]
-        tag4.references.append(da2)
-        tag4.references.append(da3)
-        tag4.extent = [1, 1]
-        tag4.position = [0.5, 0.5]
-        self.validator.check_tag(tag4, 3, 0)
-        tag_dim_warn1 = ("Number of extent and dimensionality of reference "
-                         "do not match")
-        tag_dim_warn2 = ("Number of position and dimensionality of reference "
-                         "do not match")
-        tagerr = self.validator.errors['blocks'][0]['tags'][3]['errors']
-        assert tag_dim_warn2 in tagerr
-        assert tag_dim_warn1 in tagerr
+        err_unit_match = ("some of the referenced DataArrays' dimensions "
+                          "have units that are not convertible to the units "
+                          "set in the Tag "
+                          "(Note: composite units are not supported)")
+        tag.units = ("V", "A", "L")
+        res = self.file.validate()
+        tagerr = res["errors"][tag]
+        assert err_unit_match in tagerr
 
-        tag5 = self.block2.tags[0]
-        da2 = self.block1.data_arrays[1]
-        da2.append_range_dimension([1, 2, 3, 4, 5, 6, 7, 8, 9])
-        da2.dimensions[0]._h5group.set_attr("unit", "s")
-        da2.append_sampled_dimension(0.5)
-        da2.dimensions[1]._h5group.set_attr("unit", None)
-        tag5.references.append(da2)
-        self.validator.check_tag(tag5, 0, 1)
-        assert self.validator.errors['blocks'][1]['tags'][0]['errors'] == [
-            'Some dimensions of references have no units']
+    def test_check_tag_pos_ext_mismatch(self):
+        errmsg = ("number of entries in extent does not match "
+                  "number of dimensions in all referenced DataArrays")
+        tag = self.file.blocks[0].tags[0]
+        tag.extent = [100]
+
+        res = self.file.validate()
+        tagerr = res["errors"][tag]
+        assert errmsg in tagerr
 
     def test_check_multi_tag(self):
         mt1 = self.block1.multi_tags[0]
@@ -363,3 +422,12 @@ class TestValidate (unittest.TestCase):
         self.validator.check_sources(src1, 0)
         err = self.validator.errors['blocks'][0]['sources']
         assert "Name of Source is missing" in err
+
+    @staticmethod
+    def print_all_results(res):
+        print("Errors")
+        for obj, msg in res["errors"].items():
+            print("  {}: {}".format(obj.name, msg))
+        print("Warnings")
+        for obj, msg in res["warnings"].items():
+            print("  {}: {}".format(obj.name, msg))
