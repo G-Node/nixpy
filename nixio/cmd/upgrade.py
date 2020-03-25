30e9a01eb834724b11f50dded454fb9c5751853f
Achilleas Koutsou <achilleas.k@gmail.com>
2020-03-25 14:11:32 +0100

    Upgrade script: Add file ID and update file version
    This is a WIP and still needs to take care of old style Property
    objects.
    
    The script first checks the file header version to see if it's older
    than the current. If it is, it continues to collect tasks that need to
    be performed to complete the upgrade.
    
    Tasks are created using functions that check if a change is required and
    if so, they return a closure bound to the specific filename. The
    returned function is then run after the user confirms they want to apply
    the changes. If --force is specified, the changes are applied without
    prompting.
    
    All checks and changes are made in standalone functions that open the
    file using h5py and close it immediately after. This makes each change
    atomic (or atomic-ish) and reduces the chances that a file will be left
    in an invalid state. There's still a chance the user will stop the
    procedure after a change has been applied and before the file version in
    the header is updated. This will put the file in a sort of invalid state
    but rerunning the upgrade script should finalise the changes.
    
    Regardless of the possibilities of corruption or invalid states, the
    user is warned to make sure their files and data are backed up.

--- nixio/cmd/upgrade.py
+++ nixio/cmd/upgrade.py
@@ -1,4 +1,66 @@
 import argparse
+import nixio as nix
+import h5py
+
+
+def get_file_version(fname):
+    with h5py.File(fname, mode="r") as hfile:
+        return tuple(hfile.attrs["version"])
+
+
+def has_valid_file_id(fname):
+    with h5py.File(fname, mode="r") as hfile:
+        fileid = hfile.attrs.get("id")
+        if fileid and nix.util.is_uuid(fileid):
+            return True
+    return False
+
+
+def add_file_id(fname):
+    """
+    Returns a closure that binds the filename. When the return value is
+    called, it adds a UUID to the file header.
+    """
+    def add_id():
+        "Add a UUID to the file header"
+        with h5py.File(fname, mode="a") as hfile:
+            hfile.attrs["id"] = nix.util.create_id()
+    return add_id
+
+
+def update_format_version(fname):
+    """
+    Returns a closure that binds the filename. When the return value is
+    called, it updates the version in the header to the version in the library.
+    """
+    def update_ver():
+        with h5py.File(fname, mode="a") as hfile:
+            hfile.attrs["version"] = nix.file.HDF_FF_VERSION
+    lib_verstr = ".".join(str(v) for v in nix.file.HDF_FF_VERSION)
+    update_ver.__doc__ = f"Update the file format version to {lib_verstr}"
+    return update_ver
+
+
+def collect_tasks(fname):
+    file_ver = get_file_version(fname)
+    file_verstr = ".".join(str(v) for v in file_ver)
+    lib_verstr = ".".join(str(v) for v in nix.file.HDF_FF_VERSION)
+    if file_ver >= nix.file.HDF_FF_VERSION:
+        print(f"{fname}: Up to date ({file_verstr})")
+        return
+
+    # even if the version string indicates the file is old, check format
+    # details before scheduling tasks
+    tasks = list()
+    if not has_valid_file_id(fname):
+        tasks.append(add_file_id(fname))
+
+    # always update the format in the end
+    tasks.append(update_format_version(fname))
+    print(f"{fname}: {file_verstr} -> {lib_verstr}")
+    print("  - " + "\n  - ".join(t.__doc__ for t in tasks) + "\n")
+
+    return tasks
 
 
 def main():
@@ -10,9 +72,45 @@ def main():
     parser.add_argument("file", type=str, nargs="+",
                         help="path to file to upgrade (at least one)")
     args = parser.parse_args()
-    files = args.file
+    filenames = args.file
 
-    print(f"Upgrading {len(files)} files...")
+    tasks = dict()
+    for fname in filenames:
+        tasklist = collect_tasks(fname)
+        if not tasklist:
+            continue
+
+        tasks[fname] = tasklist
+
+    if not tasks:
+        return
+
+    force = args.force
+    if not force:
+        print("""
+PLEASE READ CAREFULLY
+
+If you choose to continue, the changes listed above will be applied to the
+respective files. This will make the files unreadable by older NIX library
+versions. Although this procedure is generally fast and safe, interrupting it
+may leave files in a corrupted state.
+
+MAKE SURE YOUR FILES AND DATA ARE BACKED UP BEFORE CONTINUING.
+        """)
+        conf = None
+
+        while conf not in ("y", "n", "yes", "no"):
+            conf = input("Continue with changes? [yes/no] ")
+            conf = conf.lower()
+    else:
+        conf = "yes"
+
+    if conf in ("y", "yes"):
+        for fname, tasklist in tasks.items():
+            print(f"Processing {fname} ", end="", flush=True)
+            for task in tasklist:
+                task()
+            print("done")
 
 
 if __name__ == "__main__":
