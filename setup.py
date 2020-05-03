5e8492ccc4d51541f849e78e04a6c77b42577c83
Jan Grewe <jan.grewe@g-node.org>
2020-05-04 00:34:58 +0200

    [cmd/nixexplore] rename

--- setup.py
+++ setup.py
@@ -85,6 +85,6 @@ setup(
     entry_points={'console_scripts': [
         'nixio-validate=nixio.cmd.validate:main',
         'nixio-upgrade=nixio.cmd.upgrade:main',
-        'nixio-info=nixio.cmd.nixinfo:main',
+        'nixio-explore=nixio.cmd.nixexplore:main',
     ]}
 )
