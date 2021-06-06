febf2d445d2a65602185d949bf764517dcdc105f
Jan Grewe <jan.grewe@g-node.org>
2021-06-06 16:59:58 +0200

    [docs/examples] update example code

--- docs/source/examples/irregularlySampledData.py
+++ docs/source/examples/irregularlySampledData.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 """
- Copyright © 2014 German Neuroinformatics Node (G-Node)
+ Copyright © 2014-2021 German Neuroinformatics Node (G-Node)
 
  All rights reserved.
 
@@ -17,11 +17,11 @@
 """
 import nixio as nix
 import numpy as np
-import matplotlib.pylab as plt
+import matplotlib.pyplot as plt
 
 
 def create_data(duration, interval):
-    times = np.around(np.cumsum(np.random.poisson(interval*1000, int(1.5*duration/interval)))/1000., 3)
+    times = np.around(np.cumsum(np.random.poisson(interval * 1000, int(1.5 * duration / interval))) / 1000., 3)
     times = times[times <= duration]
     x = np.arange(0, times[-1] * 2 * np.pi, 0.001)
     y = np.sin(5 * x)
@@ -32,36 +32,37 @@ def plot_data(data_array):
     x_axis = data_array.dimensions[0]
     x = list(x_axis.ticks)
     y = data_array[:]
-    plt.plot(x, y, marker='o', color='dodgerblue')
+    plt.plot(x, y, marker='o', color='dodgerblue', label=data_array.name)
     plt.xlabel(x_axis.label + " [" + x_axis.unit + "]")
     plt.ylabel(data_array.label + " [" + data_array.unit + "]")
-    plt.title(data_array.name)
-    plt.xlim([0, times[-1]])
-    plt.ylim(np.min(y)*1.1, np.max(y)*1.1)
+    plt.legend()
+    plt.xlim([0, x[-1]])
+    plt.ylim(np.min(y) * 1.1, np.max(y) * 1.1)
     plt.show()
 
 
-if __name__ == "__main__":
+def main():
     # fake some data
-    times, y = create_data(1.0, 0.02)
+    times, values = create_data(1.0, 0.02)
 
     # create a new file overwriting any existing content
-    file_name = 'irregular_data_example.h5'
+    file_name = 'irregular_data_example.nix'
     file = nix.File.open(file_name, nix.FileMode.Overwrite)
 
     # create a 'Block' that represents a grouping object. Here, the recording session.
-    # it gets a name and a type
     block = file.create_block("block name", "nix.session")
 
     # create a 'DataArray' to take the data, add some information about the signal
-    data = block.create_data_array("sinewave", "nix.irregular_sampled", data=y)
+    data = block.create_data_array("sinewave", "nix.irregular_sampled", data=values)
     data.unit = "mV"
     data.label = "voltage"
     # add a descriptor for the xaxis
-    dim = data.append_range_dimension(times)
-    dim.unit = "s"
-    dim.label = "time"
+    data.append_range_dimension(ticks=times, label="time", unit="s")
 
     # let's plot the data from the stored information
     plot_data(data)
     file.close()
+
+
+if __name__ == "__main__":
+    main()
