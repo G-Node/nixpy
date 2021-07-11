c35eeb89666cad6c229834c5408366880ec9788f
Jan Grewe <jan.grewe@g-node.org>
2021-07-11 16:43:38 +0200

    [setup] remove py2.7

--- setup.py
+++ setup.py
@@ -49,7 +49,6 @@ def get_wheel_data():
 classifiers = [
     'Development Status :: 5 - Production/Stable',
     'Programming Language :: Python',
-    'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Topic :: Scientific/Engineering'
