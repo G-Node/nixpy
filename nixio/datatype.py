9f3d24c3d42bada4a912e7ed92159330129777fb
Jan Grewe <jan.grewe@g-node.org>
2024-07-09 16:23:46 +0200

    Update datatype.py

--- nixio/datatype.py
+++ nixio/datatype.py
@@ -7,7 +7,6 @@
 # modification, are permitted under the terms of the BSD License. See
 # LICENSE file in the root of the Project.
 from numbers import Integral, Real
-from six import string_types
 from packaging import version
 
 import numpy as np
