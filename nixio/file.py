5e5ec01f0662d20b380301a187702cd11012fb3c
wendtalexander <96268796+wendtalexander@users.noreply.github.com>
2025-03-29 11:14:36 +0100

    Pathlib for nixpy  (#550)
    * [pathlib] adding pathlibsupport
    
    * [multitag] removing docs, and check, should not be in this branch
    
    * [file_test] adding test for pathlib and unicode
    
    * [file] adding pahtlib support, removing check for utf-8 encoding
    
    * [multi_tag] removing embed
    
    * [file] removing embed
    
    * [test] moving test class to test_file.py
    
    * [pathlib] adding pathlibsupport
    
    * [multitag] removing docs, and check, should not be in this branch
    
    * [file_test] adding test for pathlib and unicode
    
    * [file] adding pahtlib support, removing check for utf-8 encoding
    
    * [multi_tag] removing embed
    
    * [file] removing embed
    
    * [test] moving test class to test_file.py
    
    * [file] fixing imports

--- nixio/file.py
+++ nixio/file.py
@@ -6,24 +6,24 @@
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted under the terms of the BSD License. See
 # LICENSE file in the root of the Project.
-import os
+
 import gc
-import numpy as np
+import pathlib
 from sys import maxsize
+from typing import Union
 from warnings import warn
 
 import h5py
+import numpy as np
 
-from .hdf5.h5group import H5Group
+from . import util, validator
 from .block import Block
-from .section import Section
-from .container import Container, SectionContainer
-from . import util
-from .exceptions import InvalidFile, DuplicateName
-from .util import find as finders
-from . import validator
 from .compression import Compression
-
+from .container import Container, SectionContainer
+from .exceptions import DuplicateName, InvalidFile
+from .hdf5.h5group import H5Group
+from .section import Section
+from .util import find as finders
 
 FILE_FORMAT = "nix"
 HDF_FF_VERSION = (1, 2, 1)
@@ -81,7 +81,7 @@ def make_fcpl():
 
 class File:
 
-    def __init__(self, path, mode=FileMode.ReadWrite,
+    def __init__(self, path: Union[str, pathlib.Path], mode=FileMode.ReadWrite,
                  compression=Compression.Auto,
                  auto_update_timestamps=True):
         """
@@ -96,27 +96,24 @@ class File:
 
         :return: nixio.File object
         """
-        try:
-            path = path.encode("utf-8")
-        except (UnicodeError, LookupError):
-            pass
+        path = pathlib.Path(path)
 
-        if not os.path.exists(path) and mode == FileMode.ReadOnly:
+        if not path.exists() and mode == FileMode.ReadOnly:
             raise RuntimeError(
                 "Cannot open non-existent file in ReadOnly mode!"
             )
 
-        if not os.path.exists(path) or mode == FileMode.Overwrite:
+        if not path.exists or mode == FileMode.Overwrite:
             mode = FileMode.Overwrite
             h5mode = map_file_mode(mode)
-            fid = h5py.h5f.create(path, flags=h5mode, fapl=make_fapl(),
+            fid = h5py.h5f.create(str(path).encode("utf-8"), flags=h5mode, fapl=make_fapl(),
                                   fcpl=make_fcpl())
             self._h5file = h5py.File(fid)
             self._root = H5Group(self._h5file, "/", create=True)
             self._create_header()
         else:
             h5mode = map_file_mode(mode)
-            fid = h5py.h5f.open(path, flags=h5mode, fapl=make_fapl())
+            fid = h5py.h5f.open(str(path).encode("utf-8"), flags=h5mode, fapl=make_fapl())
             self._h5file = h5py.File(fid)
             self._root = H5Group(self._h5file, "/")
 
@@ -143,6 +140,7 @@ class File:
         if backend is not None:
             warn("Backend selection is deprecated. Ignoring value.")
         return cls(path, mode, compression, auto_update_timestamps)
+
 
     def _create_header(self):
         self._set_format()
--- nixio/test/test_file.py
+++ nixio/test/test_file.py
@@ -9,6 +9,7 @@
 import os
 import time
 import h5py
+import pathlib
 import unittest
 import numpy as np
 
@@ -327,3 +328,22 @@ class TestFileVer(unittest.TestCase):
 
         self.set_header(version=(1, 0, 0), fileid="")
         self.try_open(nix.FileMode.ReadOnly)
+
+
+class TestFilePathlib(unittest.TestCase):
+
+    def setUp(self):
+        self.tmpdir = TempDir("filetest")
+        self.testfilename = pathlib.Path(self.tmpdir.path) / "filetest.nix"
+
+    def tearDown(self):
+        self.tmpdir.cleanup()
+
+    def test_with_pathlib(self):
+        file = nix.File.open(self.testfilename, nix.FileMode.Overwrite)
+        file.close()
+
+    def test_with_unicode(self):
+        file_test = self.testfilename.parent /"👍_test.nix"
+        file = nix.File.open(file_test, nix.FileMode.Overwrite)
+        file.close()
